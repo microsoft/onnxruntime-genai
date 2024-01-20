bbd2370a71b1a21f4b4ccecb633b3392e873d36a
Changming Sun <chasun@microsoft.com>
2024-01-20 01:46:27 +0000

    clang-tidy fixes

--- src/ort_genai_c.cpp
+++ src/ort_genai_c.cpp
@@ -1,8 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 #include "ort_genai_c.h"
+#include <memory>
+#include <onnxruntime_c_api.h>
+#include <exception>
+#include <cstdint>
+#include <cstddef>
 #include "generators.h"
-#include "search.h"
 #include "models/model.h"
 
 namespace Generators {
@@ -10,17 +14,18 @@ namespace Generators {
 std::unique_ptr<OrtEnv> g_ort_env;
 
 OrtEnv& GetOrtEnv() {
-  if (!g_ort_env)
+  if (!g_ort_env) {
     g_ort_env = OrtEnv::Create();
+}
   return *g_ort_env;
 }
 
-}
+}  // namespace Generators
 
 extern "C" {
 
 struct OgaResult {
-  OgaResult(const char *what){}
+  explicit OgaResult(const char *what){}
  // TODO: implement this constructor !!!!
 };
 
