fcc1f6a51f4bdb83c99f865cd385279f6cacaef0
Jian Chen <cjian@microsoft.com>
2024-02-02 14:39:04 -0800

    Adding wheel and pip

--- tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
@@ -5,7 +5,7 @@ os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
 
 echo "installing for CentOS version : $os_major_version"
 dnf install -y \
-  python311-devel python311-pip python311-wheel\
+  python3-devel python3-pip python3-wheel\
   glibc-langpack-\* glibc-locale-source which redhat-lsb-core \
   expat-devel tar unzip zlib-devel make bzip2 bzip2-devel \
   java-11-openjdk-devel graphviz \
