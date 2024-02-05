d531880963fe4b240a6aa25139f13365fd7c23dc
Nat Kershaw <nakersha@microsoft.com>
2024-02-05 11:21:28 -0800

    Update README

--- install_ort.bat
+++
@@ -1,6 +0,0 @@
-copy C:\code\github\onnxruntime\include\onnxruntime\core\session\onnxruntime_c_api.h ort\.
-copy C:\code\github\onnxruntime\build\Windows\RelWithDebInfo\RelWithDebInfo\onnxruntime.lib ort\.
-copy C:\code\github\onnxruntime\build\Windows\RelWithDebInfo\RelWithDebInfo\onnxruntime.dll ort\.
-copy C:\code\github\onnxruntime\build\Windows\RelWithDebInfo\RelWithDebInfo\onnxruntime_providers_shared.dll ort\.
-copy C:\code\github\onnxruntime\build\Windows\RelWithDebInfo\RelWithDebInfo\onnxruntime_providers_cuda.dll ort\.
-
--- install_ort.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ort/
-
-cp /(please change)/onnxruntime/include/onnxruntime/core/session/onnxruntime_c_api.h ort/
-cp /(please change)/onnxruntime/build/Linux/Release/libonnxruntime*.so* ort/
