8649c3402ffdaffced402875d150de70626d04cf
Jian Chen <cjian@microsoft.com>
2024-02-08 17:33:41 -0500

    Merging with main

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -4,5 +4,6 @@ option(USE_CUDA "Build with CUDA support" ON)
 option(USE_TOKENIZER "Build with Tokenizer support" ON)
 option(ENABLE_PYTHON "Enable python buildings" ON)
 option(ENABLE_UNIT_TESTS "Enable unit tests" ON)
+option(ENABLE_TESTS "Enable tests" ON)
 
 cmake_dependent_option(BUILD_WHEEL "Build the python wheel" ON "ENABLE_PYTHON" OFF)
