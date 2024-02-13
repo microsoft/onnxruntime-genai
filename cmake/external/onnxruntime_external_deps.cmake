0155313eb9d08745c2f9506a4b68ddba2fcdbb51
Jian Chen <cjian@microsoft.com>
2024-02-12 23:14:46 -0500

    Reformat cmake files. (#75)

--- cmake/external/onnxruntime_external_deps.cmake
+++ cmake/external/onnxruntime_external_deps.cmake
@@ -20,19 +20,19 @@ endforeach()
 message("Loading Dependencies ...")
 
 if(ENABLE_PYTHON)
-FetchContent_Declare(
+  FetchContent_Declare(
     pybind11_project
     URL ${DEP_URL_pybind11}
     URL_HASH SHA1=${DEP_SHA1_pybind11}
     FIND_PACKAGE_ARGS 2.6 NAMES pybind11
-)
-onnxruntime_fetchcontent_makeavailable(pybind11_project)
+  )
+  onnxruntime_fetchcontent_makeavailable(pybind11_project)
 
-if(TARGET pybind11::module)
-  set(pybind11_lib pybind11::module)
-else()
-  set(pybind11_dep pybind11::pybind11)
-endif()
+  if(TARGET pybind11::module)
+    set(pybind11_lib pybind11::module)
+  else()
+    set(pybind11_dep pybind11::pybind11)
+  endif()
 endif()
 
 FetchContent_Declare(
