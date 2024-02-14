b3e820ede65939142be5ba161e5cde9dd98948c8
Jian Chen <cjian@microsoft.com>
2024-02-13 21:16:09 -0500

    making vcpkg optional

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -4,5 +4,6 @@ option(USE_CUDA "Build with CUDA support" ON)
 option(USE_TOKENIZER "Build with Tokenizer support" ON)
 option(ENABLE_PYTHON "Enable python buildings" ON)
 option(ENABLE_TESTS "Enable tests" ON)
+option(USE_VCPKG "Use vcpkg" OFF)
 
 cmake_dependent_option(BUILD_WHEEL "Build the python wheel" ON "ENABLE_PYTHON" OFF)
