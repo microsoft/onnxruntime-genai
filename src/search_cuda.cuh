b11074f5f2c1f7635d43d72148c45ddcf3d7a0cd
aciddelgado <139922440+aciddelgado@users.noreply.github.com>
2024-02-22 08:05:00 -0800

    Accelerate CUDA Top 1 Select (#102)
    Previous implementation did not parallelize across logits in a batch.
    Average performance measured on RTX 4090 over 1000 iterations using
    vocab size 32000.
    
    | Average performance per batch size | Cuda Argmax (unbatched) | Custom
    Sampling Kernel (batched) |
    |--------------------|---------------|--------------|
    | Batch Size 1          | 24.62μs          | 22.86μs        |
    | Batch Size 5       | 63.30μs         | 25.55μs        |
    | Batch Size 12          | 120.35μs           | 23.6μs          |

--- src/search_cuda.cuh
+++ src/search_cuda.cuh
@@ -6,7 +6,6 @@ struct ArgMaxData {
   virtual ~ArgMaxData() = default;
 };
 
-void Launch_ArgMax(std::unique_ptr<ArgMaxData>& data, int32_t* next_tokens, const float* next_token_scores, int batch_size, int vocab_size, cudaStream_t stream);
 void Launch_CheckForEOS(int32_t* next_tokens, int next_tokens_count, bool* eos_meet, int eos_token_id, int pad_token_id, bool* done_cpu, cudaStream_t stream);
 void LaunchAddProbsKernel(float* log_probs, float* cum_log_probs, const int batch_size, const int num_beams, const int vocab_size, cudaStream_t stream);
 void LaunchRepetitionPenaltyProcessor(const int32_t* sequences, float* next_token_scores, int batch_size, int num_beams, int vocab_size, int max_sequence_length, int current_sequence_length, float repetition_penalty, cudaStream_t stream);
