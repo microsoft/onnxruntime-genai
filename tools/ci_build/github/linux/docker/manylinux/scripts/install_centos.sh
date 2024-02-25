fea2ecc4e0324fe04ba4b0e97f483e1b515e7bcd
Jian Chen <cjian@microsoft.com>
2024-02-25 17:50:16 -0500

    Dockerfile.manylinux2_28_cuda

--- tools/ci_build/github/linux/docker/manylinux/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/manylinux/scripts/install_centos.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-set -e
+set -e -x
 
 os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
 
-echo "installing for os major version : $os_major_version"
-dnf install -y glibc-langpack-\*
-yum install -y which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel perl-IPC-Cmd openssl-devel wget
-
-# export PATH=/opt/python/cp38-cp38/bin:$PATH
-
-echo "installing rapidjson for AzureEP"
-wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz
-tar zxvf v1.1.0.tar.gz
-cd rapidjson-1.1.0
-mkdir build
-cd build
-cmake ..
-cmake --install .
-cd ../..
+echo "installing for CentOS version : $os_major_version"
+dnf install -y \
+  glibc-langpack-\* glibc-locale-source which redhat-lsb-core \
+  expat-devel tar unzip zlib-devel make bzip2 bzip2-devel \
+  gcc-toolset-12-binutils gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-gcc-gfortran
+locale
--- tools/ci_build/github/linux/docker/manylinux/scripts/install_deps.sh
+++ tools/ci_build/github/linux/docker/manylinux/scripts/install_deps.sh
@@ -2,10 +2,7 @@
 set -e -x
 pushd .
 PYTHON_EXES=("/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10" "/opt/python/cp311-cp311/bin/python3.11" "/opt/python/cp312-cp312/bin/python3.12")
-CURRENT_DIR=$(pwd)
-if ! [ -x "$(command -v protoc)" ]; then
-  $CURRENT_DIR/install_protobuf.sh
-fi
+
 popd
 export ONNX_ML=1
 export CMAKE_ARGS="-DONNX_GEN_PB_TYPE_STUBS=OFF -DONNX_WERROR=OFF"
@@ -34,3 +31,54 @@ if ! [ -x "$(command -v ccache)" ]; then
     rm -f ccache_src.tar.gz
     rm -rf ccache_src
 fi
+
+# Download a file from internet
+function GetFile {
+  local uri=$1
+  local path=$2
+  local force=${3:-false}
+  local download_retries=${4:-5}
+  local retry_wait_time_seconds=${5:-30}
+
+  if [[ -f $path ]]; then
+    if [[ $force = false ]]; then
+      echo "File '$path' already exists. Skipping download"
+      return 0
+    else
+      rm -rf "$path"
+    fi
+  fi
+
+  if [[ -f $uri ]]; then
+    echo "'$uri' is a file path, copying file to '$path'"
+    cp "$uri" "$path"
+    return $?
+  fi
+
+  echo "Downloading $uri"
+  curl "$uri" -sSL --retry $download_retries --retry-delay $retry_wait_time_seconds --create-dirs -o "$path" --fail
+  return $?
+}
+mkdir -p /tmp/src
+
+cd /tmp/src
+
+CPU_ARCH=$(uname -m)
+
+# Install CMake
+echo "Installing cmake"
+GetFile "https://github.com/Kitware/CMake/releases/download/v3.28.1/cmake-3.28.1-linux-$CPU_ARCH.tar.gz" "/tmp/src/cmake.tar.gz"
+tar -zxf /tmp/src/cmake.tar.gz --strip=1 -C /usr
+
+# Install Ninja
+echo "Installing Ninja"
+GetFile https://github.com/ninja-build/ninja/archive/v1.10.0.tar.gz /tmp/src/ninja-linux.tar.gz
+tar -zxf ninja-linux.tar.gz
+pushd ninja-1.10.0
+cmake -Bbuild-cmake -H.
+cmake --build build-cmake
+mv ./build-cmake/ninja /usr/bin
+popd
+
+cd /
+rm -rf /tmp/src
