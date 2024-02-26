3e513da34db64e74f4ed2a0a7446f8acbcb45a95
Nat Kershaw (MSFT) <nakersha@microsoft.com>
2024-02-26 07:48:42 -0800

    Update examples with current Python API, and clean up old files (#111)
    Update phi-2 and llama Python examples with latest API, both high and
    low level.
    
    Remove old files that are no longer used.
    
    Update README.
    
    ---------
    
    Co-authored-by: kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
    Co-authored-by: Yufeng Li <liyufeng1987@gmail.com>

--- examples/llama/llama-loop.py
+++ examples/llama/llama-loop.py
@@ -1,35 +1,42 @@
 ﻿import onnxruntime_genai as og
 
 print("Loading model...")
-model=og.Model("model", og.DeviceType.CPU)
+
+# The first argument is the name of the folder containing the model files
+model=og.Model("example-models/llama2-7b-chat-int4-cpu", og.DeviceType.CPU)
 print("Model loaded")
 tokenizer=model.create_tokenizer()
 print("Tokenizer created")
 
-prompt = "I like walking my cute dog"
+prompts = ["I like walking my cute dog",
+           "What is the best restaurant in town?",
+           "Hello, how are you today?"]
 
-input_tokens = tokenizer.encode(prompt)
+input_tokens = tokenizer.encode_batch(prompts)
 
-params=og.SearchParams(model)
+params=og.GeneratorParams(model)
 params.max_length = 256
-params.input_ids = input_tokens
+params.set_input_sequences(input_tokens)
+
 
 generator=og.Generator(model, params)
 
-print("Prompt:")
-print(prompt, end='', flush=True)
+print("Generator created")
+
+print("Running generation loop ...")
 
 while not generator.is_done():
     generator.compute_logits()
 
-    # search.apply_minLength(1)
-    # search.apply_repetition_penalty(1.0)
+    # Customize generation parameters
+    # TODO: these do not work yet
+    #generator.apply_min_length(1)
+    #generator.apply_repetition_penalty(1.0)
 
     generator.generate_next_token_top_p(0.9, 1.0)
 
-print("Output:")
+print("Outputs:")
 
-print(tokenizer.decode(generator.get_sequence(0).GetArray()))
-    
-print()
-print()
+for i in range(len(prompts)):
+   print(tokenizer.decode(generator.get_sequence(i).get_array()))
+   print()
