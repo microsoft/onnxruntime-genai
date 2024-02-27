89e7404b266301d75762fa48a7d65fe08150f9c0
Jian Chen <cjian@microsoft.com>
2024-02-27 14:21:10 -0500

    install_centos_gcc11

--- tools/ci_build/github/linux/docker/manylinux/scripts/install_centos_gcc11.sh
+++ tools/ci_build/github/linux/docker/manylinux/scripts/install_centos_gcc11.sh
@@ -6,6 +6,6 @@ os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
 echo "installing for CentOS version : $os_major_version"
 dnf install -y \
   glibc-langpack-\* glibc-locale-source which redhat-lsb-core \
-  expat-devel tar unzip zlib-devel make bzip2 bzip2-devel \
-  gcc-toolset-11-binutils gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ gcc-toolset-11-gcc-gfortran
+  perl-IPC-Cmd openssl-devel wget \
+  expat-devel tar unzip zlib-devel make bzip2 bzip2-devel
 locale
