9caabd3474f7efff1dc53ad1fa9354c1aa408643
Baiju Meswani <bmeswani@microsoft.com>
2024-02-29 09:43:55 -0800

    Add sample python tests for the CI pipeline (#128)

--- test/python/test_onnxruntime_genai_api.py
+++ test/python/test_onnxruntime_genai_api.py
@@ -13,11 +13,15 @@ import pytest
 # onnxruntime-genai and onnxruntime. This introduces incompatibility.
 # @pytest.mark.parametrize("device", [og.DeviceType.CPU, og.DeviceType.CUDA] if og.is_cuda_available() else [og.DeviceType.CPU])
 @pytest.mark.parametrize("device", [og.DeviceType.CPU])
-@pytest.mark.parametrize("relative_model_path", [Path("hf-internal-testing") / "tiny-random-gpt2-fp32", Path("hf-internal-testing") / "tiny-random-gpt2-fp32"])
+@pytest.mark.parametrize(
+    "relative_model_path",
+    [
+        Path("hf-internal-testing") / "tiny-random-gpt2-fp32",
+        Path("hf-internal-testing") / "tiny-random-gpt2-fp32",
+    ],
+)
 def test_greedy_search(device, test_data_path, relative_model_path):
-    model_path = os.fspath(
-        Path(test_data_path) / relative_model_path
-    )
+    model_path = os.fspath(Path(test_data_path) / relative_model_path)
 
     model = og.Model(model_path, device)
 
@@ -50,29 +54,86 @@ def test_greedy_search(device, test_data_path, relative_model_path):
     for i in range(len(sequences)):
         assert sequences[i] == expected_sequence[i].tolist()
 
-'''
-TODO: Enable once the phi-2 model exists
 
 @pytest.mark.parametrize("device", [og.DeviceType.CPU])
-@pytest.mark.parametrize("relative_model_path", [Path("phi-2")])
-def test_batching(device, test_data_path, relative_model_path):
-    model_path = os.fspath(
-        Path(test_data_path) / relative_model_path
-    )
+@pytest.mark.parametrize(
+    "relative_model_path", [Path("hf-internal-testing") / "tiny-random-gpt2-fp32"]
+)
+@pytest.mark.parametrize("batch_encode", [True, False])
+@pytest.mark.parametrize("batch_decode", [True, False])
+def test_tokenizer_encode_decode(
+    device, test_data_path, relative_model_path, batch_encode, batch_decode
+):
+    model_path = os.fspath(Path(test_data_path) / relative_model_path)
 
     model = og.Model(model_path, device)
     tokenizer = model.create_tokenizer()
 
     prompts = [
-      "This is a test.",
-      "Rats are awesome pets!",
-      "The quick brown fox jumps over the lazy dog.",
+        "This is a test.",
+        "Rats are awesome pets!",
+        "The quick brown fox jumps over the lazy dog.",
+    ]
+    sequences = None
+    if batch_encode:
+        sequences = tokenizer.encode_batch(prompts)
+    else:
+        sequences = [tokenizer.encode(prompt) for prompt in prompts]
+
+    decoded_strings = None
+    if batch_decode:
+        decoded_strings = tokenizer.decode_batch(sequences)
+    else:
+        decoded_strings = [tokenizer.decode(sequence) for sequence in sequences]
+
+    assert prompts == decoded_strings
+
+
+@pytest.mark.parametrize("device", [og.DeviceType.CPU])
+@pytest.mark.parametrize(
+    "relative_model_path", [Path("hf-internal-testing") / "tiny-random-gpt2-fp32"]
+)
+def test_tokenizer_stream(device, test_data_path, relative_model_path):
+    model_path = os.fspath(Path(test_data_path) / relative_model_path)
+
+    model = og.Model(model_path, device)
+    tokenizer = model.create_tokenizer()
+    tokenizer_stream = tokenizer.create_stream()
+
+    prompts = [
+        "This is a test.",
+        "Rats are awesome pets!",
+        "The quick brown fox jumps over the lazy dog.",
+    ]
+    sequences = tokenizer.encode_batch(prompts)
+
+    for index, sequence in enumerate(sequences):
+        decoded_string = ""
+        for token in sequence:
+            decoded_string += tokenizer_stream.decode(token)
+
+        assert decoded_string == prompts[index]
+
+
+# TODO: Enable once the phi-2 model exists
+@pytest.mark.skip(reason="Phi-2 model does not exist in the pipeline.")
+@pytest.mark.parametrize("device", [og.DeviceType.CPU])
+@pytest.mark.parametrize("relative_model_path", [Path("phi-2")])
+def test_batching(device, test_data_path, relative_model_path):
+    model_path = os.fspath(Path(test_data_path) / relative_model_path)
+
+    model = og.Model(model_path, device)
+    tokenizer = model.create_tokenizer()
+
+    prompts = [
+        "This is a test.",
+        "Rats are awesome pets!",
+        "The quick brown fox jumps over the lazy dog.",
     ]
 
-    params=og.GeneratorParams(model)
+    params = og.GeneratorParams(model)
     params.set_search_options({"max_length":20}) # To run faster
     params.set_input_sequences(tokenizer.encode_batch(prompts))
 
     output_sequences = model.generate(params)
     print(tokenizer.decode_batch(output_sequences))
-'''
