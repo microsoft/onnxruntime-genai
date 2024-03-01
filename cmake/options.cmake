ac53234868bdd942298e9ed6808db1ec435dac00
Jian Chen <cjian@microsoft.com>
2024-03-01 12:22:24 -0800

    -DMANYLINUX=ON

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -5,5 +5,6 @@ option(NO_TOKENIZER "Don't include the Tokenizer" OFF)
 option(ENABLE_PYTHON "Build the Python API." ON)
 option(ENABLE_TESTS "Enable tests" OFF)
 option(TEST_PHI2 "Enable tests for Phi2" ON)
+option(MANYLINUX "Build manylinux wheels" OFF)
 
 cmake_dependent_option(BUILD_WHEEL "Build the python wheel" ON "ENABLE_PYTHON" OFF)
