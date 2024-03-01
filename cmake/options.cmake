3521eda74790f6025ca86be73e5e6cd891a6cec5
Jian Chen <cjian@microsoft.com>
2024-03-01 09:56:58 -0800

    Managed

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,7 +3,7 @@ include(CMakeDependentOption)
 option(USE_CUDA "Build with CUDA support" ON)
 option(NO_TOKENIZER "Don't include the Tokenizer" OFF)
 option(ENABLE_PYTHON "Build the Python API." ON)
-option(ENABLE_TESTS "Enable tests" ON)
+option(ENABLE_TESTS "Enable tests" OFF)
 option(TEST_PHI2 "Enable tests for Phi2" ON)
 
 cmake_dependent_option(BUILD_WHEEL "Build the python wheel" ON "ENABLE_PYTHON" OFF)
