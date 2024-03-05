4c0b4b16269904c7842c4965681648ce23b0c0a8
Ryan Hill <ryanhill@microsoft.com>
2024-03-05 02:05:48 -0800

    Lint

--- src/models/logits.h
+++ src/models/logits.h
@@ -15,8 +15,8 @@ struct Logits {
 
   std::array<int64_t, 3> shape_{};
   ONNXTensorElementDataType type_;
-  std::unique_ptr<OrtValue> value32_; // Always fp32 values
-  std::unique_ptr<OrtValue> value16_; // When model output is fp16
+  std::unique_ptr<OrtValue> value32_;  // Always fp32 values
+  std::unique_ptr<OrtValue> value16_;  // When model output is fp16
 };
 
 }  // namespace Generators
