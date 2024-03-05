85f496e1493f914d523317ae630e772e8093d4b5
Jian Chen <cjian@microsoft.com>
2024-03-05 15:30:18 -0800

    remove USE_CXX17

--- src/span.h
+++ src/span.h
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 #pragma once
-#ifndef USE_CXX17
+#if __cplusplus >= 202002L
 #include <span>
 #else
 #include <array>
