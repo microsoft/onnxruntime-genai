3db906b0d27d5b44e5872c3911c23eb99e4bf1d9
sa_ddam213 <sa_ddam213@live.com>
2024-03-16 11:36:19 +1300

    fix lint

--- src/ort_genai_c.h
+++ src/ort_genai_c.h
@@ -185,7 +185,7 @@ OGA_EXPORT OgaResult* OGA_API_CALL OgaGenerator_GenerateNextToken_Top(OgaGenerat
 OGA_EXPORT OgaResult* OGA_API_CALL OgaGenerator_GenerateNextToken_TopK(OgaGenerator* generator, int k, float t);
 
 /*Top-P sampling selects words from the smallest set of words whose cumulative probability exceeds a predefined threshold (p)
-*/
+ */
 OGA_EXPORT OgaResult* OGA_API_CALL OgaGenerator_GenerateNextToken_TopP(OgaGenerator* generator, float p, float t);
 
 OGA_EXPORT OgaResult* OGA_API_CALL OgaGenerator_GenerateNextToken_TopK_TopP(OgaGenerator* generator, int k, float p, float t);
