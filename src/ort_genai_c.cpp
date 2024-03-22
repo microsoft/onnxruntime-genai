efc5ee8b48a2d3ee492c046f2bd33ec7d7290a14
Ryan Hill <ryanhill@microsoft.com>
2024-03-22 14:14:04 -0700

    Easy to use C++ API Wrapper

--- src/ort_genai_c.cpp
+++ src/ort_genai_c.cpp
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#include "ort_genai_c.h"
 #include <memory>
 #include <onnxruntime_c_api.h>
-#include <exception>
+#include <stdexcept>
 #include <cstdint>
 #include <cstddef>
+#include "span.h"
+#include "ort_genai_c.h"
 #include "generators.h"
 #include "models/model.h"
 #include "search.h"
@@ -22,6 +23,11 @@ OrtEnv& GetOrtEnv() {
   return *g_ort_env;
 }
 
+struct Result {
+  explicit Result(const char* what) : what_{what} {}
+  std::string what_;
+};
+
 }  // namespace Generators
 
 extern "C" {
@@ -30,16 +36,11 @@ extern "C" {
 #define OGA_CATCH                   \
   }                                 \
   catch (const std::exception& e) { \
-    return new OgaResult{e.what()}; \
+    return reinterpret_cast<OgaResult*>(std::make_unique<Generators::Result>(e.what()).release()); \
   }
 
-struct OgaResult {
-  explicit OgaResult(const char* what) : what_{what} {}
-  std::string what_;
-};
-
-const char* OGA_API_CALL OgaResultGetError(OgaResult* result) {
-  return result->what_.c_str();
+const char* OGA_API_CALL OgaResultGetError(const OgaResult* result) {
+  return reinterpret_cast<const Generators::Result*>(result)->what_.c_str();
 }
 
 OgaResult* OGA_API_CALL OgaCreateSequences(OgaSequences** out) {
