eda329539a70e1a926fba5ccfa383b7a1111227c
Adam Clark <sa_ddam213@live.com>
2024-04-17 05:42:30 +1200

    Windows Example/Debug UI (#252)

---
+++ examples/csharp/Genny/Genny/ViewModel/ResultModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Genny.ViewModel
+{
+    public class ResultModel : INotifyPropertyChanged
+    {
+        private string _content;
+        private bool _isUserInput;
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; NotifyPropertyChanged(); }
+        }
+
+        public bool IsUserInput
+        {
+            get { return _isUserInput; }
+            set { _isUserInput = value; NotifyPropertyChanged(); }
+        }
+
+        public DateTime Timestamp { get; } = DateTime.Now;
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion
+    }
+}
