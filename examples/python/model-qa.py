a74fa1adfdd22b4e020fbbfa5d8109af4a99a3aa
Ryan Hill <ryanhill@microsoft.com>
2024-04-18 15:01:32 -0700

    Remove extra }

--- examples/python/model-qa.py
+++ examples/python/model-qa.py
@@ -27,7 +27,7 @@ def main(args):
         input_tokens = tokenizer.encode(args.system_prompt + text)
 
         params = og.GeneratorParams(model)
-        params.set_search_options(do_sample=False, max_length=args.max_length, min_length=args.min_length, top_p=args.top_p, top_k=args.top_k, temperature=args.temperature, repetition_penalty=args.repetition_penalty})
+        params.set_search_options(do_sample=False, max_length=args.max_length, min_length=args.min_length, top_p=args.top_p, top_k=args.top_k, temperature=args.temperature, repetition_penalty=args.repetition_penalty)
         params.input_ids = input_tokens
         generator = og.Generator(model, params)
         if args.verbose: print("Generator created")
