410a1eaad5e8de3461622b3a595074ce79092ad1
Patrice Vignola <vignola.patrice@gmail.com>
2024-04-23 14:57:23 -0700

    Fix

--- src/models/position_inputs.cpp
+++ src/models/position_inputs.cpp
@@ -303,7 +303,11 @@ void PositionInputs::UpdateAttentionMask(int current_length) {
       throw std::runtime_error("PositionIDs::Update - Unsupported device type");
   }
 
-#if !USE_DML
+#if USE_DML
+  if (model_.device_type_ != DeviceType::DML) {
+    attention_mask_ = std::move(attention_mask_next_);
+  }
+#else
   attention_mask_ = std::move(attention_mask_next_);
 #endif
 
