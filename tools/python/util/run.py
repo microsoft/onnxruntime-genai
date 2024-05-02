e7cb4770fec0dd530836484741a57605d04c2c76
Scott McKay <Scott.McKay@microsoft.com>
2024-05-02 19:17:20 +1000

    Merge AndroidBuild changes

--- tools/python/util/run.py
+++ tools/python/util/run.py
@@ -1,29 +1,31 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import logging
-import os
+from __future__ import annotations  # for '|' shorthand in type hints
+
 import shlex
 import subprocess
+from pathlib import Path
+from .logger import get_logger
 
-_log = logging.getLogger("util.run")
+_log = get_logger("util.run")
 
 
 def run(
     *args,
-    cwd=None,
+    cwd: Path | None = None,
     input=None,
-    capture_stdout=False,
-    capture_stderr=False,
-    shell=False,
-    env=None,
-    check=True,
-    quiet=False,
+    capture_stdout: bool = False,
+    capture_stderr: bool = False,
+    shell: bool = False,
+    env: dict[str, str] | None = None,
+    check: bool = True,
+    quiet: bool = False,
 ):
     """Runs a subprocess.
 
     Args:
-        *args: The subprocess arguments.
+        *args: The subprocess arguments. May be passed as a single argument containing a list of strings.
         cwd: The working directory. If None, specifies the current directory.
         input: The optional input byte sequence.
         capture_stdout: Whether to capture stdout.
@@ -37,18 +39,22 @@ def run(
     Returns:
         A subprocess.CompletedProcess instance.
     """
-    cmd = [*args]
+    if len(args) == 1 and isinstance(args[0], list):
+        cmd = args[0]
+    else:
+        cmd = [*args]
 
-    _log.info(
-        "Running subprocess in '{}'\n  {}".format(cwd or os.getcwd(), " ".join([shlex.quote(arg) for arg in cmd]))
-    )
+    if not cwd:
+        cwd = Path.cwd()
+
+    _log.info(f"Running subprocess in '{cwd}'\n  {' '.join([shlex.quote(arg) for arg in cmd])}")
 
     def output(is_stream_captured):
         return subprocess.PIPE if is_stream_captured else (subprocess.DEVNULL if quiet else None)
 
     completed_process = subprocess.run(
         cmd,
-        cwd=cwd,
+        cwd=str(cwd),
         check=check,
         input=input,
         stdout=output(capture_stdout),
