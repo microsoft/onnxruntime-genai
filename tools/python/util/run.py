1298c2fb2f8cbf6365281bc103980bbf052a5acc
Scott McKay <Scott.McKay@microsoft.com>
2024-05-02 15:20:02 +1000

    Fix issue with old versions of python

--- tools/python/util/run.py
+++ tools/python/util/run.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from __future__ import annotations  # for '|' shorthand in type hints
+
 import shlex
 import subprocess
 from pathlib import Path
