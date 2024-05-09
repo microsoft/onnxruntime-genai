2641dcf9486c1087168b6470595492e1361b1dba
Scott McKay <Scott.McKay@microsoft.com>
2024-05-09 14:11:47 +1000

    Address PR comments

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-python3 ./build.py "$@"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+python3 $SCRIPT_DIR/build.py "$@"
