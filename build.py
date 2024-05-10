11dec9b80522116f199363da0cca7a1d62e4fcfd
Scott McKay <Scott.McKay@microsoft.com>
2024-05-11 08:45:28 +1000

    PR comments

--- build.py
+++ build.py
@@ -352,8 +352,8 @@ def build(args: argparse.Namespace, env: dict[str, str]):
         # Build the library
         csharp_build_command = [dotnet, "build", ".",]
         csharp_build_command += _get_csharp_properties(args)
-        util.run(csharp_build_command, cwd=str(REPO_ROOT / "src" / "csharp"))
-        util.run(csharp_build_command, cwd=str(REPO_ROOT / "test" / "csharp"))
+        util.run(csharp_build_command, cwd=REPO_ROOT / "src" / "csharp")
+        util.run(csharp_build_command, cwd=REPO_ROOT / "test" / "csharp")
 
 
 def test(args: argparse.Namespace, env: dict[str, str]):
