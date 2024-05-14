15ea047f6d97bfdf618a0692fa1d66f66bdc0203
Scott McKay <Scott.McKay@microsoft.com>
2024-05-14 15:34:33 +1000

    Test can now load model

--- src/java/src/test/java/ai/onnxruntime_genai/SimpleGenAITest.java
+++ src/java/src/test/java/ai/onnxruntime_genai/SimpleGenAITest.java
@@ -7,16 +7,24 @@ package ai.onnxruntime_genai;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.File;
+import java.net.URL;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
 public class SimpleGenAITest {
   private static final Logger logger = Logger.getLogger(SimpleGenAITest.class.getName());
 
+  private static final String testModelPath() {
+    URL url = SimpleGenAI.class.getResource("/hf-internal-testing/tiny-random-gpt2-fp32");
+    File f = new File(url.getFile());
+    return f.getPath();
+  }
+
   @Test
   public void testUsage() throws GenAIException {
-    SimpleGenAI generator =
-        new SimpleGenAI("test_models/hf-internal-testing/tiny-random-gpt2-fp32");
+    System.out.println(testModelPath());
+    SimpleGenAI generator = new SimpleGenAI(testModelPath());
     GeneratorParams params = generator.createGeneratorParams("This is a testing prompt");
     params.setSearchOption("early_stopping", true);
     params.setSearchOption("whatthe", 123);
@@ -25,8 +33,7 @@ public class SimpleGenAITest {
 
   @Test
   public void testInvalidSearchOption() throws GenAIException {
-    SimpleGenAI generator =
-        new SimpleGenAI("test_models/hf-internal-testing/tiny-random-gpt2-fp32");
+    SimpleGenAI generator = new SimpleGenAI(testModelPath());
     GeneratorParams params = generator.createGeneratorParams("This is a testing prompt");
     assertThrows(GenAIException.class, () -> params.setSearchOption("invalid", true));
     assertFalse(true);
