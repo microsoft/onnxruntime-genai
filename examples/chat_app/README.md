91dc682858adc065e3ba6db2447fccda250793ec
Xiang Zhang <xianz@microsoft.com>
2024-05-14 09:08:46 -0700

    add LLM chat app into ort_genai examples

---
+++ examples/chat_app/README.md
@@ -0,0 +1,63 @@
+# LLM Chat UI <!-- omit in toc -->
+This is a chat demo using the various versions of the LLMs
+
+**Contents**:
+- [set up](#set-up)
+- [Launch the app](#launch-the-app)
+
+# set up
+
+1. Install **onnxruntime-genai-directml** 
+   
+   ```
+   pip install numpy
+   pip install --pre onnxruntime-genai-directml
+   ```
+
+   Or following [Build onnxruntime-genai from source](https://onnxruntime.ai/docs/genai/howto/build-from-source.html#build-onnxruntime-genai-from-source)
+
+2. Install the requirements
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+# Launch the app
+
+1. Create folder named models at the root directory of chat_app.
+
+2. Download models to the created folder, take phi-3-mini directml as example.
+
+    ```bash
+    huggingface-cli download microsoft/Phi-3-mini-4k-instruct-onnx --include directml/* --local-dir .
+    ```
+
+     File structure should look as the below:
+    ```
+    --chat_app
+    --models
+        --directml
+            --phi-3-mini-directml-int4-awq-block-128
+            --meta-llama_Llama-2-7b-chat-hf
+            --mistralai_Mistral-7B-Instruct-v0.1
+            ...
+    ```
+
+
+
+
+3. Launch the app
+
+    ```
+    python chat_app/app.py
+    ```
+
+    You should see output from console
+    ```
+    Running on local URL:  http://127.0.0.1:7860
+
+        To create a public link, set `share=True` in `launch()`.
+    ```
+
+   Then open the local URL in broswer
+   ![alt text](image.png)
---
+++ examples/chat_app/interface/hddr_llm_onnx_dml_interface.py
@@ -0,0 +1,182 @@
+import gc
+import os
+import sys
+import onnxruntime_genai as og
+from app_modules.utils import convert_to_markdown, is_stop_word_or_prefix, shared_state
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(current_dir, "..", "..", ".."))
+
+
+class ONNXModel():
+    """A wrapper for OnnxRuntime-GenAI to run ONNX LLM model."""
+
+    def __init__(self, model_path):
+        self.og = og
+        self.model = og.Model(f'{model_path}')
+        self.tokenizer = og.Tokenizer(self.model)
+        self.tokenizer_stream = self.tokenizer.create_stream()
+        self.is_phi = "phi" in model_path
+        if self.is_phi:
+            self.enable_history_max = 10 if "mini" in model_path else 2
+            self.history_template = "<|user|>{input}<|end|><|assistant|>{response}<|end|>"
+            self.chat_template = "<|user|>{input}<|end|><|assistant|>"
+        else:
+            self.enable_history_max = 2
+            self.history_template = "[INST]{input}[/INST]{response}"
+            self.chat_template = "[INST]{input}[/INST]"
+
+    def generate_prompt_with_history(self, text, history, max_length=2048):
+        prompt = ""
+
+        for dialog in history[-self.enable_history_max:]:
+            prompt += f'{self.history_template.format(input=dialog[0], response=dialog[1])}'
+
+        if not self.is_phi:
+            prompt = f"<s>{prompt}</s>"
+
+        prompt += f'{self.chat_template.format(input=text)}'
+
+        input_ids = self.tokenizer.encode(prompt + f'{self.chat_template.format(input=text)}')
+
+        if len(input_ids) <= max_length:
+            return input_ids
+        else:
+            history.clear()
+            return self.tokenizer.encode(f'{self.chat_template.format(input=text)}')
+
+    def greedy_search(
+        self,
+        input_ids,
+        max_length: int,
+        token_printing_step: int = 4,
+    ):
+        output_tokens = []
+
+        params = og.GeneratorParams(self.model)
+        params.try_use_cuda_graph_with_max_batch_size(1)
+        params.input_ids = input_ids
+
+        search_options = {"max_length" : max_length}
+        params.set_search_options(**search_options)
+
+        generator = og.Generator(self.model, params)
+
+        idx = 0
+        while not generator.is_done():
+            idx += 1
+            generator.compute_logits()
+            generator.generate_next_token()
+            next_token = generator.get_next_tokens()[0]
+            output_tokens.append(next_token)
+
+            if idx % token_printing_step == 0:
+                yield self.tokenizer.decode(output_tokens)
+
+    def predict(
+        self,
+        text,
+        chatbot,
+        history,
+        max_length_tokens,
+        max_context_length_tokens,
+        token_printing_step,
+        *args
+    ):
+        if text == "":
+            yield chatbot, history, "Empty context."
+            return
+
+        inputs = self.generate_prompt_with_history(
+            text, history, max_length=max_context_length_tokens
+        )
+
+        if inputs is None:
+            yield chatbot, history, "Input too long."
+            return
+
+        input_ids = inputs[-max_context_length_tokens:]
+
+        human_tokens = [
+            "[|Human|]",
+            "Human:",
+            "### HUMAN:",
+            "### User:",
+            "USER:",
+            "<|im_start|>user",
+            "<|user|>",
+            "### Instruction:",
+            "GPT4 Correct User:",
+        ]
+
+        ai_tokens = [
+            "[|AI|]",
+            "AI:",
+            "### RESPONSE:",
+            "### Response:",
+            "ASSISTANT:",
+            "<|im_start|>assistant",
+            "<|assistant|>",
+            "GPT4 Correct Assistant:",
+            "### Assistant:",
+        ]
+
+        for x in self.greedy_search(
+            input_ids,
+            max_length=max_length_tokens,
+            token_printing_step=token_printing_step,
+        ):
+            sentence = x
+
+            if is_stop_word_or_prefix(sentence, ["[|Human|]", "[|AI|]", "Human:", "AIL"]) is False:
+                for human_token in human_tokens:
+                    if human_token in sentence:
+                        sentence = sentence[: sentence.index(human_token)].strip()
+                        break
+
+                for ai_token in ai_tokens:
+                    if ai_token in sentence:
+                        sentence = sentence[: sentence.index(ai_token)].strip()
+                        break
+                sentence = sentence.strip()
+                a, b = [[y[0], convert_to_markdown(y[1])] for y in history] + [[text, convert_to_markdown(sentence)]], [
+                    *history,
+                    [text, sentence],
+                ]
+                yield a, b, "Generating..."
+
+            if shared_state.interrupted:
+                shared_state.recover()
+                try:
+                    yield a, b, "Stop: Success"
+                    return
+                except Exception as e:
+                    print(type(e).__name__, e)
+
+        del input_ids
+        gc.collect()
+
+        try:
+            yield a, b, "Generate: Success"
+        except Exception as e:
+            print(type(e).__name__, e)
+
+        return
+    
+    def shutdown(self):
+        pass
+    
+    def retry(self, chatbot, history, max_length_tokens, max_context_length_tokens, token_printing_step):
+        if len(history) == 0:
+            yield chatbot, history, "Empty context"
+            return
+        chatbot.pop()
+        inputs = history.pop()[0]
+        yield from self.predict(
+            inputs,
+            chatbot,
+            history,
+            max_length_tokens,
+            max_context_length_tokens,
+            token_printing_step,
+        )
