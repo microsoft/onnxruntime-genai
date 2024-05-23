f57a6712ca557f4b941d9dfe9fd7fdad87aa1452
Chester Liu <4710575+skyline75489@users.noreply.github.com>
2024-05-23 08:36:24 +0800

    Remove std::filesystem for the compatibility of old systems (#480)
    From https://github.com/microsoft/onnxruntime-extensions/pull/721

--- src/models/decoder_only.cpp
+++ src/models/decoder_only.cpp
@@ -4,7 +4,7 @@
 namespace Generators {
 DecoderOnly_Model::DecoderOnly_Model(std::unique_ptr<Config> config, OrtEnv& ort_env)
     : Model{std::move(config)} {
-  session_decoder_ = OrtSession::Create(ort_env, (config_->config_path / config_->model.decoder.filename).c_str(), session_options_.get());
+  session_decoder_ = OrtSession::Create(ort_env, (config_->config_path / fs::path(config_->model.decoder.filename)).c_str(), session_options_.get());
 
   InitDeviceAllocator(*session_decoder_);
 }
