9f327d4ae9a02bd7d6c3f8616858deb5077e1fb4
Scott McKay <Scott.McKay@microsoft.com>
2024-06-19 10:22:31 +1000

    Revert direct usage of ORT in SimpleTest.kt

--- src/java/src/test/android/app/src/androidTest/java/ai/onnxruntime_genai/example/javavalidator/SimpleTest.kt
+++ src/java/src/test/android/app/src/androidTest/java/ai/onnxruntime_genai/example/javavalidator/SimpleTest.kt
@@ -1,7 +1,7 @@
 package ai.onnxruntime.genai.example.javavalidator
 
-import ai.onnxruntime.OrtEnvironment
-import ai.onnxruntime.OrtProvider
+// import ai.onnxruntime.OrtEnvironment
+// import ai.onnxruntime.OrtProvider
 import ai.onnxruntime.genai.*
 import android.os.Build
 import android.util.Log
@@ -26,10 +26,10 @@ class SimpleTest {
     @Before
     fun start() {
         Log.i(TAG, "SystemABI=" + Build.SUPPORTED_ABIS[0])
-        val providers = OrtEnvironment.getAvailableProviders()
-        for (provider in providers) {
-            Log.i(TAG, "Providers=$provider")
-        }
+        // val providers = OrtEnvironment.getAvailableProviders()
+        // for (provider in providers) {
+        //     Log.i(TAG, "Providers=$provider")
+        // }
     }
 
     @Throws(IOException::class)
