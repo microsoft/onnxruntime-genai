711a69ad053426afa51e8259367f8c9a05693125
Scott McKay <Scott.McKay@microsoft.com>
2024-06-21 20:51:12 +1000

    Add Tensor to Java bindings. Add setModelInput to GeneratorParams. Add doco on adding new Java bindings.

--- src/java/src/main/java/ai/onnxruntime/genai/SimpleGenAI.java
+++ src/java/src/main/java/ai/onnxruntime/genai/SimpleGenAI.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
 package ai.onnxruntime.genai;
 
 import java.util.function.Consumer;
@@ -55,6 +59,10 @@ public class SimpleGenAI {
    * Create the generator parameters and add the prompt text. The user can set other search options
    * via the GeneratorParams object prior to running `generate`.
    *
+   * @param tokenIds The encoded token ids for the prompt/s.
+   * @param sequenceLength The length of each sequence in tokenIds. All sequences must have the same
+   *     length.
+   * @param batchSize The number of batches in tokenIds.
    * @return The generator parameters.
    * @throws GenAIException on failure
    */
