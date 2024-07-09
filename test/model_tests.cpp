1f10c564fcd24ae19714c0c53f4c20e43b762812
aciddelgado <aciddelgado@microsoft.com>
2024-07-09 11:30:59 -0700

    tests work and pass

--- test/model_tests.cpp
+++ test/model_tests.cpp
@@ -56,56 +56,46 @@ TEST(ModelTests, GreedySearchGptFp32) {
   }
 }
 
-// TEST(ModelTests, BeamSearchGptFp32) {
-//   std::vector<int64_t> input_ids_shape{3, 12};
-//   std::vector<int32_t> input_ids{
-//       0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
-//       41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572,
-//       0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328};
+TEST(ModelTests, BeamSearchGptFp32) {
+  std::vector<int64_t> input_ids_shape{3, 12};
+  std::vector<int32_t> input_ids{
+      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
+      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572,
+      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328};
 
-//   std::vector<int32_t> expected_output{
-//       0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620, 131, 131, 131, 181, 638, 638, 638, 638,
-//       41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 292, 292, 292, 292, 292, 292, 292, 292,
-//       0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 328, 669, 669, 669, 669, 669, 669, 669};
+  std::vector<int32_t> expected_output{
+      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620, 131, 131, 131, 181, 638, 638, 638, 638,
+      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 292, 292, 292, 292, 292, 292, 292, 292,
+      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 328, 669, 669, 669, 669, 669, 669, 669};
 
-//   // The ONNX model is generated like the following:
-//   // python convert_generation.py --model_type gpt2 -m hf-internal-testing/tiny-random-gpt2
-//   //        --output tiny_gpt2_beamsearch_fp16.onnx --use_gpu --max_length 20
-//   // (with separate_gpt2_decoder_for_init_run set to False as it is now set to True by default)
+  // The ONNX model is generated like the following:
+  // python convert_generation.py --model_type gpt2 -m hf-internal-testing/tiny-random-gpt2
+  //        --output tiny_gpt2_beamsearch_fp16.onnx --use_gpu --max_length 20
+  // (with separate_gpt2_decoder_for_init_run set to False as it is now set to True by default)
 
-//   auto model = Generators::CreateModel(Generators::GetOrtEnv(), MODEL_PATH "hf-internal-testing/tiny-random-gpt2-fp32");
+  auto model = Generators::CreateModel(Generators::GetOrtEnv(), MODEL_PATH "hf-internal-testing/tiny-random-gpt2-fp32");
 
-//   auto params = Generators::CreateGeneratorParams(*model);
-//   params->batch_size = static_cast<int>(input_ids_shape[0]);
-//   params->sequence_length = static_cast<int>(input_ids_shape[1]);
-//   params->input_ids = input_ids;
-//   params->search.max_length = 20;
-//   params->search.length_penalty = 1.0f;
-//   params->search.num_beams = 4;
+  auto params = Generators::CreateGeneratorParams(*model);
+  params->batch_size = static_cast<int>(input_ids_shape[0]);
+  params->sequence_length = static_cast<int>(input_ids_shape[1]);
+  params->input_ids = input_ids;
+  params->search.max_length = 20;
+  params->search.length_penalty = 1.0f;
+  params->search.num_beams = 4;
 
-//   Generators::BeamSearch_Cpu search{*params};
-//   auto state = model->CreateState(search.sequence_lengths_, *params);
+  auto generator = Generators::CreateGenerator(*model, *params);
+  auto result = Generators::Generate(*model, *params);
 
-//   while (!search.IsDone()) {
-//     search.SetLogits(state->Run(search.GetSequenceLength(), search.GetNextTokens(), search.GetNextIndices()));
+  // std::cout << "Expected output: " << tokenizer->Decode(expected_output) << "\r\n";
+  // std::cout << "Actual output: " << tokenizer->Decode(result[0]) << "\r\n";
 
-//     // Scoring
-//     search.ApplyMinLength(1);
-//     search.ApplyRepetitionPenalty(1.0f);
-
-//     search.SelectTop();
-//   }
-
-//   std::vector<int32_t> output_sequence(static_cast<size_t>(search.params_->batch_size) * search.params_->search.max_length);
-//   search.Finalize(1, Generators::cpu_span<int32_t>{output_sequence}, {});
-
-//   // Verify outputs match expected outputs
-//   for (size_t i = 0; i < static_cast<size_t>(search.params_->batch_size); i++) {
-//     auto sequence = std::span<int32_t>(output_sequence.data() + search.params_->search.max_length * i, search.params_->search.max_length);
-//     auto* expected_output_start = &expected_output[i * search.params_->search.max_length];
-//     EXPECT_TRUE(0 == std::memcmp(expected_output_start, sequence.data(), params->search.max_length * sizeof(int32_t)));
-//   }
-// }
+  // Verify outputs match expected outputs
+  for (int i = 0; i < params->batch_size; i++) {
+    auto sequence = std::span<int32_t>(result[i].data(), params->search.max_length);
+    auto* expected_output_start = &expected_output[i * params->search.max_length];
+    EXPECT_TRUE(0 == std::memcmp(expected_output_start, sequence.data(), params->search.max_length * sizeof(int32_t)));
+  }
+}
 #endif
 
 #if USE_CUDA
@@ -147,59 +137,50 @@ TEST(ModelTests, GreedySearchGptCuda) {
     Test_GreedySearch_Gpt_Cuda(model_path.first, model_path.second);
 }
 
-// void Test_BeamSearch_Gpt_Cuda(const char* model_path, const char* model_label) {
-//   std::vector<int64_t> input_ids_shape{3, 12};
-//   std::vector<int32_t> input_ids{
-//       0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
-//       41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572,
-//       0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328};
+void Test_BeamSearch_Gpt_Cuda(const char* model_path, const char* model_label) {
+  std::vector<int64_t> input_ids_shape{3, 12};
+  std::vector<int32_t> input_ids{
+      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
+      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572,
+      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328};
 
-//   std::vector<int32_t> expected_output{
-//       0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620, 131, 131, 131, 181, 638, 638, 638, 638,
-//       41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 292, 292, 292, 292, 292, 292, 292, 292,
-//       0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 328, 669, 669, 669, 669, 669, 669, 669};
+  std::vector<int32_t> expected_output{
+      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620, 131, 131, 131, 181, 638, 638, 638, 638,
+      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 292, 292, 292, 292, 292, 292, 292, 292,
+      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 328, 669, 669, 669, 669, 669, 669, 669};
 
-//   // The ONNX model is generated like the following:
-//   // python convert_generation.py --model_type gpt2 -m hf-internal-testing/tiny-random-gpt2
-//   //        --output tiny_gpt2_beamsearch_fp16.onnx --use_gpu --max_length 20
-//   // (with separate_gpt2_decoder_for_init_run set to False as it is now set to True by default)
-//   auto model = Generators::CreateModel(Generators::GetOrtEnv(), model_path);
+  // The ONNX model is generated like the following:
+  // python convert_generation.py --model_type gpt2 -m hf-internal-testing/tiny-random-gpt2
+  //        --output tiny_gpt2_beamsearch_fp16.onnx --use_gpu --max_length 20
+  // (with separate_gpt2_decoder_for_init_run set to False as it is now set to True by default)
+  auto model = Generators::CreateModel(Generators::GetOrtEnv(), model_path);
 
-//   auto params = Generators::CreateGeneratorParams(*model);
-//   params->batch_size = static_cast<int>(input_ids_shape[0]);
-//   params->sequence_length = static_cast<int>(input_ids_shape[1]);
-//   params->input_ids = input_ids;
-//   params->search.max_length = 20;
-//   params->search.num_beams = 4;
-//   params->search.length_penalty = 1.0f;
+  auto params = Generators::CreateGeneratorParams(*model);
+  params->batch_size = static_cast<int>(input_ids_shape[0]);
+  params->sequence_length = static_cast<int>(input_ids_shape[1]);
+  params->input_ids = input_ids;
+  params->search.max_length = 20;
+  params->search.num_beams = 4;
+  params->search.length_penalty = 1.0f;
 
-//   auto generator = Generators::CreateGenerator(*model, *params);
+  auto generator = Generators::CreateGenerator(*model, *params);
+  auto result = Generators::Generate(*model, *params);
 
-//   while (!generator->IsDone()) {
-//     generator->ComputeLogits();
-//     generator->GenerateNextToken();
-//   }
+  // std::cout << "Expected output: " << tokenizer->Decode(expected_output) << "\r\n";
+  // std::cout << "Actual output: " << tokenizer->Decode(result[0]) << "\r\n";
 
-//   size_t sequence_length = params->batch_size * params->search.max_length;
-//   auto output_sequence_cuda = Generators::CudaMallocArray<int32_t>(sequence_length);
-//   auto output_sequence_cpu = std::make_unique<int32_t[]>(sequence_length);
+  // Verify outputs match expected outputs
+  for (int i = 0; i < params->batch_size; i++) {
+    auto sequence = std::span<int32_t>(result[i].data(), params->search.max_length);
+    auto* expected_output_start = &expected_output[i * params->search.max_length];
+    EXPECT_TRUE(0 == std::memcmp(expected_output_start, sequence.data(), params->search.max_length * sizeof(int32_t)));
+  }
+}
 
-//   generator->search_->Finalize(1, Generators::gpu_span<int32_t>(output_sequence_cuda.get(), sequence_length), {});
-//   cudaMemcpyAsync(output_sequence_cpu.get(), output_sequence_cuda.get(), sequence_length * sizeof(int32_t), cudaMemcpyDeviceToHost, params->cuda_stream);
-//   cudaStreamSynchronize(params->cuda_stream);
-
-//   // Verify outputs match expected outputs
-//   for (int i = 0; i < params->batch_size; i++) {
-//     auto sequence = std::span<int32_t>(output_sequence_cpu.get() + params->search.max_length * i, params->search.max_length);
-//     auto* expected_output_start = &expected_output[i * params->search.max_length];
-//     EXPECT_TRUE(0 == std::memcmp(expected_output_start, sequence.data(), params->search.max_length * sizeof(int32_t)));
-//   }
-// }
-
-// TEST(ModelTests, BeamSearchGptCuda) {
-//   for (auto model_path : c_tiny_gpt2_model_paths)
-//     Test_BeamSearch_Gpt_Cuda(model_path.first, model_path.second);
-// }
+TEST(ModelTests, BeamSearchGptCuda) {
+  for (auto model_path : c_tiny_gpt2_model_paths)
+    Test_BeamSearch_Gpt_Cuda(model_path.first, model_path.second);
+}
 
 TEST(ModelTests, TestApiCuda) {
 #if TEST_PHI2
