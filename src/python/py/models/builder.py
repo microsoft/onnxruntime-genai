1b223d13f8557572a6181bb966238eae0531462f
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2024-07-09 02:50:05 +0000

    Fix config updating

--- src/python/py/models/builder.py
+++ src/python/py/models/builder.py
@@ -1875,7 +1875,6 @@ class Model:
             from peft import AutoPeftModelForCausalLM
             extra_kwargs = {} if os.path.exists(input_path) else {"num_hidden_layers": self.num_layers} if "num_hidden_layers" in self.extra_options else {"cache_dir": self.cache_dir}
             model = AutoPeftModelForCausalLM.from_pretrained(input_path, use_auth_token=True, trust_remote_code=True, **extra_kwargs)
-            # self.matmul_attrs["use_lora"] = True
         else:
             # Load PyTorch model
             extra_kwargs = {"num_hidden_layers": self.num_layers} if "num_hidden_layers" in self.extra_options else {}
@@ -2712,8 +2711,8 @@ class Phi3Small8KModel(Model):
         #            DownProjAdd
 
         # Make input MatMul and Add nodes
-        up_matmul_name = f"/model/layers.{layer_id}/mlp/up_proj/MatMul"
-        self.make_matmul(mlp.up_proj, up_matmul_name, root_input)
+        up_matmul_basename = f"/model/layers.{layer_id}/mlp/up_proj/MatMul"
+        up_matmul_name = self.make_matmul(mlp.up_proj.weight.detach().numpy(), up_matmul_basename, root_input)
         up_add_name = f"/model/layers.{layer_id}/mlp/up_proj/Add"
         self.make_add_bias(mlp.up_proj.bias.detach().numpy(), up_add_name, f"{up_matmul_name}/output_0")
 
@@ -2758,8 +2757,8 @@ class Phi3Small8KModel(Model):
         self.make_mul(mul_name, mul_inputs, dtype=self.io_dtype, shape=["batch_size", "sequence_length", self.intermediate_size])
 
         # Make output MatMul and Add nodes
-        down_matmul_name = f"/model/layers.{layer_id}/mlp/down_proj/MatMul"
-        self.make_matmul(mlp.down_proj, down_matmul_name, f"{mul_name}/output_0")
+        down_matmul_basename = f"/model/layers.{layer_id}/mlp/down_proj/MatMul"
+        down_matmul_name = self.make_matmul(mlp.down_proj.weight.detach().numpy(), down_matmul_basename, f"{mul_name}/output_0")
         down_add_name = f"/model/layers.{layer_id}/mlp/down_proj/Add"
         self.make_add_bias(mlp.down_proj.bias.detach().numpy(), down_add_name, f"{down_matmul_name}/output_0")
 
@@ -2860,14 +2859,15 @@ def create_model(model_name, input_path, output_dir, precision, execution_provid
     hf_token = parse_hf_token(extra_options.get("hf_token", "true"))
 
     is_peft = "adapter_config.json" in os.listdir(input_path)
-    peft_config = {}
+    peft_config = None
     if is_peft:
         from peft import PeftConfig
         peft_config = PeftConfig.from_pretrained(hf_name, use_auth_token=True, trust_remote_code=True, **extra_kwargs)
         hf_name = peft_config.base_model_name_or_path
 
     config = AutoConfig.from_pretrained(hf_name, token=hf_token, trust_remote_code=True, **extra_kwargs)
-    config.update(peft_config.__dict__)
+    if is_peft:
+        config.update(peft_config.__dict__)
 
     # Set input/output precision of ONNX model
     io_dtype = TensorProto.FLOAT if precision in {"int8", "fp32"} or (precision == "int4" and execution_provider == "cpu") else TensorProto.FLOAT16
