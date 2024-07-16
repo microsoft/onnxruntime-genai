bc84e1de84cb86a9187c79fb455fcc534c2016a1
Dmitri Smirnov <dmitrism@microsoft.com>
2024-07-16 11:50:28 -0700

    Add import for lora helpers

--- tools/python/util/__init__.py
+++ tools/python/util/__init__.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 from .logger import get_logger
+from .lora_helpers import save_lora_params_to_flatbuffers
 from .run import run
 from .android import *
 from .platform_helpers import is_linux, is_mac, is_windows
