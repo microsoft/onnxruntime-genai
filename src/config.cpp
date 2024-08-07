f3a77dadf94ea35e1e7b61f0533c1470137671f5
Chester Liu <4710575+skyline75489@users.noreply.github.com>
2024-08-07 14:19:10 +0800

    Initial support for QNN EP (#723)
    Added support for QNN EP and related session options

--- src/config.cpp
+++ src/config.cpp
@@ -64,6 +64,10 @@ struct SessionOptions_Element : JSON::Element {
       v_.log_id = value;
     else if (name == "enable_profiling")
       v_.enable_profiling = value;
+    else if (name == "ep_context_embed_mode")
+      v_.ep_context_embed_mode = value;
+    else if (name == "ep_context_file_path")
+      v_.ep_context_file_path = value;
     else
       throw JSON::unknown_value_error{};
   }
@@ -84,6 +88,14 @@ struct SessionOptions_Element : JSON::Element {
       v_.enable_cpu_mem_arena = value;
     else if (name == "enable_mem_pattern")
       v_.enable_mem_pattern = value;
+    else if (name == "disable_cpu_ep_fallback")
+      v_.disable_cpu_ep_fallback = value;
+    else if (name == "disable_quant_qdq")
+      v_.disable_quant_qdq = value;
+    else if (name == "enable_quant_qdq_cleanup")
+      v_.enable_quant_qdq_cleanup = value;
+    else if (name == "ep_context_enable")
+      v_.ep_context_enable = value;
     else
       throw JSON::unknown_value_error{};
   }
