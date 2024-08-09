3ac992d29697135f4cc33e8fdcd00736e3ef17f3
Abhishek Jindal <abjindal@microsoft.com>
2024-08-08 17:00:33 -0700

    cast to float

--- test/c_api_tests.cpp
+++ test/c_api_tests.cpp
@@ -193,7 +193,7 @@ TEST(CAPITests, GetOutputCAPI) {
 
   auto generator = OgaGenerator::Create(*model, *params);
   generator->ComputeLogits();
-  auto prompt_logits = generator->GetOutput("logits")->Data();
+  auto prompt_logits = static_cast<float*>(generator->GetOutput("logits")->Data());
 
   // check prompt
   // full logits has shape [2, 4, 1000]. Sample 1 for every 200 tokens and the expected sampled logits has shape [2, 4, 5]
@@ -215,7 +215,7 @@ TEST(CAPITests, GetOutputCAPI) {
   }
 
   generator->GenerateNextToken();
-  auto token_gen_logits = generator->GetOutput("logits")->Data();
+  auto token_gen_logits = static_cast<float*>(generator->GetOutput("logits")->Data();)
 
   // check for the 1st token generation
   // full logits has shape [2, 1, 1000]. Sample 1 for every 200 tokens and the expected sampled logits has shape [2, 1, 5]
