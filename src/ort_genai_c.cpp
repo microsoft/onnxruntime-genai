13f3410d64dc897ac10380e4bcd5f8d405f5c1ca
Cem Moluluo <cemmoluluo@gmail.com>
2024-08-09 10:07:30 +0000

    Minor code changes

--- src/ort_genai_c.cpp
+++ src/ort_genai_c.cpp
@@ -58,12 +58,12 @@ OgaResult* OGA_API_CALL OgaCreateSequences(OgaSequences** out) {
   OGA_CATCH
 }
 
-OgaResult* OGA_API_CALL OgaAppendTokenSequence(const int32_t * token_ptr, int32_t token_cnt, OgaSequences* sequence) {
+OgaResult* OGA_API_CALL OgaAppendTokenSequence(const int32_t * token_ptr, uint32_t token_cnt, OgaSequences* sequence) {
   OGA_TRY
   Generators::TokenSequences* toks = reinterpret_cast<Generators::TokenSequences*>(sequence);
   std::vector<int32_t> tmp(token_cnt);
   for (size_t i = 0; i < token_cnt; i++) {
-    tmp[i] = (token_ptr[i]);
+    tmp[i] = token_ptr[i];
   }
   toks->emplace_back(std::move(tmp));
   return nullptr;
