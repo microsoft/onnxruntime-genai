19e97748fe7a3271afde08a99eb7b3f23274dff4
Abhishek Jindal <abjindal@microsoft.com>
2024-08-09 12:00:55 -0700

    fix typo

--- src/ort_genai_c.cpp
+++ src/ort_genai_c.cpp
@@ -226,7 +226,7 @@ OgaResult* OGA_API_CALL OgaGenerator_GetOutput(const OgaGenerator* oga_generator
     cudaMemcpy(ortvalue_clone->GetTensorMutableRawData(), ortvalue_output->GetTensorMutableRawData(), data_size, cudaMemcpyDeviceToHost);
 #endif
   } else if (ortvalue_output->GetTensorMemoryInfo().GetDeviceType() == OrtMemoryInfoDeviceType_GPU && generator.model_->device_type_ == Generators::DeviceType::DML) {
-if USE_DML
+#if USE_DML
     std::cout << "OrtValue Output on DML device" << std::endl;
     ComPtr<ID3D12Resource> gpu_resource;
     Ort::ThrowOnError(generator.model_->GetOrtDmlApi()->GetD3D12ResourceFromAllocation(
