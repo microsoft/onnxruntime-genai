8736bfa16ee3e0e3572fc12ebd8780ac954b5797
camike <camike_huihui@sina.com>
2024-08-12 10:33:14 +0800

    clang format

--- src/dml/dml_helpers.cpp
+++ src/dml/dml_helpers.cpp
@@ -233,63 +233,56 @@ static UINT64 DMLCalcBufferTensorSize(
     DML_TENSOR_DATA_TYPE dataType,
     UINT dimensionCount,
     _In_reads_(dimensionCount) const UINT* sizes,
-    _In_reads_opt_(dimensionCount) const UINT* strides)
-{
-    UINT elementSizeInBytes = 0;
-    switch (dataType)
-    {
+    _In_reads_opt_(dimensionCount) const UINT* strides) {
+  UINT elementSizeInBytes = 0;
+  switch (dataType) {
     case DML_TENSOR_DATA_TYPE_FLOAT32:
     case DML_TENSOR_DATA_TYPE_UINT32:
     case DML_TENSOR_DATA_TYPE_INT32:
-        elementSizeInBytes = 4;
-        break;
+      elementSizeInBytes = 4;
+      break;
 
     case DML_TENSOR_DATA_TYPE_FLOAT16:
     case DML_TENSOR_DATA_TYPE_UINT16:
     case DML_TENSOR_DATA_TYPE_INT16:
-        elementSizeInBytes = 2;
-        break;
+      elementSizeInBytes = 2;
+      break;
 
     case DML_TENSOR_DATA_TYPE_UINT8:
     case DML_TENSOR_DATA_TYPE_INT8:
-        elementSizeInBytes = 1;
-        break;
+      elementSizeInBytes = 1;
+      break;
 
     case DML_TENSOR_DATA_TYPE_FLOAT64:
     case DML_TENSOR_DATA_TYPE_UINT64:
     case DML_TENSOR_DATA_TYPE_INT64:
-        elementSizeInBytes = 8;
-        break;
+      elementSizeInBytes = 8;
+      break;
 
     default:
-        return 0; // Invalid data type
+      return 0;  // Invalid data type
+  }
+
+  UINT64 minimumImpliedSizeInBytes = 0;
+  if (!strides) {
+    minimumImpliedSizeInBytes = sizes[0];
+    for (UINT i = 1; i < dimensionCount; ++i) {
+      minimumImpliedSizeInBytes *= sizes[i];
+    }
+    minimumImpliedSizeInBytes *= elementSizeInBytes;
+  } else {
+    UINT indexOfLastElement = 0;
+    for (UINT i = 0; i < dimensionCount; ++i) {
+      indexOfLastElement += (sizes[i] - 1) * strides[i];
     }
 
-    UINT64 minimumImpliedSizeInBytes = 0;
-    if (!strides)
-    {
-        minimumImpliedSizeInBytes = sizes[0];
-        for (UINT i = 1; i < dimensionCount; ++i)
-        {
-            minimumImpliedSizeInBytes *= sizes[i];
-        }
-        minimumImpliedSizeInBytes *= elementSizeInBytes;
-    }
-    else
-    {
-        UINT indexOfLastElement = 0;
-        for (UINT i = 0; i < dimensionCount; ++i)
-        {
-            indexOfLastElement += (sizes[i] - 1) * strides[i];
-        }
+    minimumImpliedSizeInBytes = (static_cast<UINT64>(indexOfLastElement) + 1) * elementSizeInBytes;
+  }
 
-        minimumImpliedSizeInBytes = (static_cast<UINT64>(indexOfLastElement) + 1) * elementSizeInBytes;
-    }
+  // Round up to the nearest 4 bytes.
+  minimumImpliedSizeInBytes = (minimumImpliedSizeInBytes + 3) & ~3ull;
 
-    // Round up to the nearest 4 bytes.
-    minimumImpliedSizeInBytes = (minimumImpliedSizeInBytes + 3) & ~3ull;
-
-    return minimumImpliedSizeInBytes;
+  return minimumImpliedSizeInBytes;
 }
 
 ComPtr<IDMLCompiledOperator> CreateCastOperator(
