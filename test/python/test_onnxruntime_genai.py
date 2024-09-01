90d09f2d283594febfa0054d77b53595ff3fd562
Yufeng Li <liyufeng1987@gmail.com>
2024-09-01 15:18:05 -0700

    add more print

--- test/python/test_onnxruntime_genai.py
+++ test/python/test_onnxruntime_genai.py
@@ -86,6 +86,8 @@ def main():
     ):
         log.debug(f"{os.path.abspath('test')} is writable: {os.access(os.path.abspath('test'), os.W_OK)}")
         log.debug(f"{os.path.abspath('build')} is writable: {os.access(os.path.abspath('build'), os.W_OK)}")
+        log.debug(f"{os.path.abspath('test/test_models/phi-2/int4/')} is writable: {os.access(os.path.abspath('test/test_models/phi-2/int4/'), os.W_OK)}")
+        log.debug(f"{os.path.abspath('test/test_models/phi-2/int4/cpu')} is writable: {os.access(os.path.abspath('test/test_models/phi-2/int4/cpu'), os.W_OK)}")
         output_paths += download_models(os.path.abspath(args.test_models), "int4", "cpu")
         if og.is_cuda_available():
             output_paths += download_models(os.path.abspath(args.test_models), "int4", "cuda")
