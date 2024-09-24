4d4884120fe7aa88f1d80f5b378032aec47196b0
ayissi-msft <t-jayissi@microsoft.com>
2024-09-24 13:24:09 -0700

    Removing calculate_perplexity logic

--- test/python/model_validation/validation_tool.py
+++ test/python/model_validation/validation_tool.py
@@ -7,52 +7,52 @@ import torch
 import json
 import tqdm
  
-def calculate_perplexity(args, output_dir):
-    print("We are now calculating perplexity")
+# def calculate_perplexity(args, output_dir):
+#     print("We are now calculating perplexity")
  
-    # actual model object
-    model = og.Model(f'{output_dir}')
+#     # actual model object
+#     model = og.Model(f'{output_dir}')
 
-    # tokenizer should already be created
-    tokenizer = og.Tokenizer(model)
-    print(tokenizer)
+#     # tokenizer should already be created
+#     tokenizer = og.Tokenizer(model)
+#     print(tokenizer)
 
-    test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+#     test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
 
-    print(test)
-    # Calculate the perplexity, understand the output of the models
-    # Understand the output from a model and logits.
-    print("Test is now loaded")
+#     print(test)
+#     # Calculate the perplexity, understand the output of the models
+#     # Understand the output from a model and logits.
+#     print("Test is now loaded")
 
-    max_length = model.config.n_positions
-    stride = 512
-    seq_len = encodings.input_ids.size(1)
+#     max_length = model.config.n_positions
+#     stride = 512
+#     seq_len = encodings.input_ids.size(1)
 
-    nlls = []
-    prev_end_loc = 0
-    for begin_loc in tqdm(range(0, seq_len, stride)):
-        end_loc = min(begin_loc + max_length, seq_len)
-        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-        input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-        target_ids = input_ids.clone()
-        target_ids[:, :-trg_len] = -100
+#     nlls = []
+#     prev_end_loc = 0
+#     for begin_loc in tqdm(range(0, seq_len, stride)):
+#         end_loc = min(begin_loc + max_length, seq_len)
+#         trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+#         input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+#         target_ids = input_ids.clone()
+#         target_ids[:, :-trg_len] = -100
 
-        with torch.no_grad():
-            outputs = model(input_ids, labels=target_ids)
+#         with torch.no_grad():
+#             outputs = model(input_ids, labels=target_ids)
 
-            # loss is calculated using CrossEntropyLoss which averages over valid labels
-            # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
-            # to the left by 1.
-            neg_log_likelihood = outputs.loss
+#             # loss is calculated using CrossEntropyLoss which averages over valid labels
+#             # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+#             # to the left by 1.
+#             neg_log_likelihood = outputs.loss
 
-        nlls.append(neg_log_likelihood)
+#         nlls.append(neg_log_likelihood)
 
-        prev_end_loc = end_loc
-        if end_loc == seq_len:
-            break
+#         prev_end_loc = end_loc
+#         if end_loc == seq_len:
+#             break
 
-    ppl = torch.exp(torch.stack(nlls).mean())
-    return
+#     ppl = torch.exp(torch.stack(nlls).mean())
+#     return
 
 
 
@@ -181,4 +181,4 @@ if __name__ == "__main__":
     '''
     6. Automatically calculate the perplexity metrics if the model has the corresponding dataset *. 
     '''
-    calculate_perplexity(args, 'output')
+    # calculate_perplexity(args, 'output')
