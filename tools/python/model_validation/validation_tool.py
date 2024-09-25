2df8e015219335395a03a4dcfea2a2bd63225ac8
ayissi-msft <t-jayissi@microsoft.com>
2024-09-25 14:08:41 -0700

    Update validation script and config files based on feedback

--- tools/python/model_validation/validation_tool.py
+++ tools/python/model_validation/validation_tool.py
@@ -1,68 +1,17 @@
 import onnxruntime_genai as og
 import argparse
 import time
-from ...src.python.py.models.builder import create_model
-from datasets import load_dataset 
-import torch
+from onnxruntime_genai.models.builder import create_model
 import json
-import tqdm
- 
-# def calculate_perplexity(args, output_dir):
-#     print("We are now calculating perplexity")
- 
-#     # actual model object
-#     model = og.Model(f'{output_dir}')
+import os
 
-#     # tokenizer should already be created
-#     tokenizer = og.Tokenizer(model)
-#     print(tokenizer)
-
-#     test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-
-#     print(test)
-#     # Calculate the perplexity, understand the output of the models
-#     # Understand the output from a model and logits.
-#     print("Test is now loaded")
-
-#     max_length = model.config.n_positions
-#     stride = 512
-#     seq_len = encodings.input_ids.size(1)
-
-#     nlls = []
-#     prev_end_loc = 0
-#     for begin_loc in tqdm(range(0, seq_len, stride)):
-#         end_loc = min(begin_loc + max_length, seq_len)
-#         trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-#         input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-#         target_ids = input_ids.clone()
-#         target_ids[:, :-trg_len] = -100
-
-#         with torch.no_grad():
-#             outputs = model(input_ids, labels=target_ids)
-
-#             # loss is calculated using CrossEntropyLoss which averages over valid labels
-#             # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
-#             # to the left by 1.
-#             neg_log_likelihood = outputs.loss
-
-#         nlls.append(neg_log_likelihood)
-
-#         prev_end_loc = end_loc
-#         if end_loc == seq_len:
-#             break
-
-#     ppl = torch.exp(torch.stack(nlls).mean())
-#     return
-
-
-
-def validate_model(args, output_dir):
+def validate_model(args, model_directory, inputs):
     if args.verbose: print("Loading model...")
     if args.timings:
         started_timestamp = 0
         first_token_timestamp = 0
 
-    model = og.Model(f'{output_dir}')
+    model = og.Model(f'{model_directory}')
     if args.verbose: print("Model loaded")
     tokenizer = og.Tokenizer(model)
     tokenizer_stream = tokenizer.create_stream()
@@ -76,10 +25,6 @@ def validate_model(args, output_dir):
     search_options['max_length'] = 512
 
     chat_template = '<|user|>\n{input} <|end|>\n<|assistant|>'
-
-
-    inputs = ["Provide a detailed analysis of the causes and consequences of the French Revolution, including key events, figures, and social changes.",
-              "Explain the process of photosynthesis in plants, detailing the chemical reactions involved, the role of chlorophyll, and the importance of sunlight"]   
     
     for input in inputs:
 
@@ -149,36 +94,41 @@ def validate_model(args, output_dir):
             print(f"Prompt length: {len(input_tokens)}, New tokens: {len(new_tokens)}, Time to first: {(prompt_time):.2f}s, Prompt tokens per second: {len(input_tokens)/prompt_time:.2f} tps, New tokens per second: {len(new_tokens)/run_time:.2f} tps")
 
 
+def folder_exists(folder_path):
+    return os.path.isdir(folder_path)
+
+def create_folder(folder_path):
+    os.mkdir(folder_path)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description="End-to-end AI Question/Answer example for gen-ai")
 
     parser.add_argument('-j', '--json', type=str, required=True, help='Path to the JSON file containing the arguments')
 
-    # parser.add_argument('-m', '--model', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Print verbose output and timing information. Defaults to false')
-    parser.add_argument('-g', '--timings', action='store_true', default=False, help='Print timing information for each generation step. Defaults to false')
-        
     args = parser.parse_args()
 
     with open(args.json, 'r') as file:
         data = json.load(file)
-        args.model = data['models']
+        models = data['models']
+        inputs = data['inputs']
 
-    '''
-    1. Download the model 
-    2. Convert to onnx format 
-    3. Create model and tokenizer from the model 
-    '''   
-    model = create_model(args.model, '', './output', 'int4', 'cpu', "./cache")
+    # Before model creation, do a check and see if the folder existsS
+    model_output_dir = "../../../models_outputs"
+    model_cache_dir = "../../../cache_models"
 
-    '''
-    4. Iterate through input texts. In each iteration, embed the input, create generator, generate output text. 
-    5. Log the input text and output text for manual evaluation. 
-    '''
-    validate_model(args, 'output')
+    if not folder_exists(model_output_dir):
+        create_folder(model_output_dir)
 
-    '''
-    6. Automatically calculate the perplexity metrics if the model has the corresponding dataset *. 
-    '''
-    # calculate_perplexity(args, 'output')
+    if not folder_exists(model_cache_dir):
+        create_folder(model_cache_dir)
+
+    for model in models:
+        # Need to give the entire length
+        onnx_model = create_model(model, '', model_output_dir, 'int4', 'cpu', model_cache_dir)
+        # Add checks after model creation
+        # validate_model(args, './models_output', inputs)
+        #Table values
+        #columns, model name, validation complete (y/n), third - exception / failure msgs
+    
+    # Print the table out once loop is completed 
