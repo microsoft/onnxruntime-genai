8f291baab4d5afd5e6bbf95fc173b4774481e51d
ayissi-msft <t-jayissi@microsoft.com>
2024-10-04 14:01:18 -0700

    adding chat template

--- tools/python/model_validation/validation_tool.py
+++ tools/python/model_validation/validation_tool.py
@@ -12,52 +12,35 @@ def create_table(output):
 
 def validate_model(config, model_directory):
     if config["verbose"]: print("Loading model...")
-    if config["timings"]:
-        started_timestamp = 0
-        first_token_timestamp = 0
 
     model = og.Model(f'{model_directory}')
 
     if config["verbose"]: print("Model loaded")
     tokenizer = og.Tokenizer(model)
-    tokenizer_stream = tokenizer.create_stream()
+    tokenizer_stream = tokenizer.create_stream() 
     if config["verbose"]: print("Tokenizer created")
-    if config["verbose"]: print()
+    if config["verbose"]: print()  
 
-    search_option_keys = [
-        'do_sample', 'max_length', 'min_length', 'top_p', 'top_k', 
-        'temperature', 'repetition_penalty'
-    ]   
-
-    search_options = {key: config[key] for key in search_option_keys} 
-
-    chat_template = '<|user|>\n{input} <|end|>\n<|assistant|>'
+    chat_template = get_chat_template(model_directory)
     
     for input in config["inputs"]:
 
         complete_text = ''
         
-        if config["timings"]: started_timestamp = time.time()
-
         prompt = f'{chat_template.format(input=input)}'
 
         input_tokens = tokenizer.encode(prompt)
 
-
         params = og.GeneratorParams(model)
-        params.set_search_options(**search_options)
         params.input_ids = input_tokens
 
         generator = og.Generator(model, params)
         if config["verbose"]: print("Generator created")
 
         if config["verbose"]: print("Running generation loop ...")
-        if config["timings"]:
-            first = True
-            new_tokens = []
 
         print()
-        # print("Output: ", end='', flush=True)
+        print("Output: ", end='', flush=True)
 
         generation_successful = True
 
@@ -65,10 +48,6 @@ def validate_model(config, model_directory):
             while not generator.is_done():
                 generator.compute_logits()
                 generator.generate_next_token()
-                if config["timings"]:
-                    if first:
-                        first_token_timestamp = time.time()
-                        first = False
 
                 new_token = generator.get_next_tokens()[0]
                 
@@ -76,9 +55,8 @@ def validate_model(config, model_directory):
 
                 complete_text += value_to_save
 
-                # print(tokenizer_stream.decode(new_token), end='', flush=True)
+                print(tokenizer_stream.decode(new_token), end='', flush=True)
                 
-                if config["timings"]: new_tokens.append(new_token)
         except KeyboardInterrupt:
             print("  --control+c pressed, aborting generation--")
             generation_successful = False
@@ -86,28 +64,20 @@ def validate_model(config, model_directory):
             print(f"An error occurred: {e}")
             generation_successful = False
 
-        print()
-        print()
-
-
-        with open('output.txt', 'a') as file:
+        with open(f'{model_directory}/output.txt', 'a') as file:
             file.write(complete_text)
 
         # Delete the generator to free the captured graph for the next generator, if graph capture is enabled
         del generator
 
-        if config["timings"]:
-            prompt_time = first_token_timestamp - started_timestamp
-            run_time = time.time() - first_token_timestamp
-            print(f"Prompt length: {len(input_tokens)}, New tokens: {len(new_tokens)}, Time to first: {(prompt_time):.2f}s, Prompt tokens per second: {len(input_tokens)/prompt_time:.2f} tps, New tokens per second: {len(new_tokens)/run_time:.2f} tps")
-
     return generation_successful
 
-def folder_exists(folder_path):
-    return os.path.isdir(folder_path)
+def get_chat_template(output_directory):
+    tokenizer_json = output_directory + '/tokenizer_config.json'
+    with open(tokenizer_json, 'r') as file:
+        config = json.load(file)
+    return config["chat_template"]
 
-def create_folder(folder_path):
-    os.mkdir(folder_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description="End-to-end AI Question/Answer example for gen-ai")
@@ -119,27 +89,38 @@ if __name__ == "__main__":
     with open(args.json, 'r') as file:
         config = json.load(file)
 
-    # Check and see if the folder exists, if not create the folder
-    model_output_dir = "../../../models_outputs/"
-    model_cache_dir = "../../../cache_models"
-
-    if not folder_exists(model_output_dir):
-        create_folder(model_output_dir)
-
-    if not folder_exists(model_cache_dir):
-        create_folder(model_cache_dir)
+    os.makedirs(config["output_directory"], exist_ok=True)
+    os.makedirs(config["cache_directory"], exist_ok=True)
 
     output = []
 
+    validation_complete = False
+
     for model in config["models"]:
-        try:
-            create_model(model, '', model_output_dir+f'/{model}', 'int4', 'cpu', model_cache_dir+f'/{model}')
-            generation_successful = validate_model(config, model_output_dir)
-            exception_message = None
-        except Exception as e:
-            exception_message = str(e)
+
+        print(f"We are validating {model}")
+        adjusted_model = model.replace("/", "_")
+        output_path = config["output_directory"] + f'/{adjusted_model}'
+        # From the output directory, there exist a file named tokenizer_config.json which contains the chat 
+        cache_path = config["cache_directory"] + f'/{adjusted_model}'
         
-        output.append([model, generation_successful, exception_message])
-    
+        try:
+            create_model(model, '', output_path, config["precision"], config["executive_provider"], cache_path)
+        except Exception as e:
+            print(f'Failure after create model {e}')
+            output.append([model, validation_complete, e])
+            continue
+        try:          
+            validation_complete = validate_model(config, output_path)
+        except Exception as e:
+            print(f'Failure after validation model {e}')
+            output.append([model, validation_complete, e])
+        
+            
     df = create_table(output)
+
+    df.to_csv("models.csv")
+
     print(df)
+
+    # From the folder name, get the chat template 
