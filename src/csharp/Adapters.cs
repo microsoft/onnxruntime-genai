e745a64c15e1ba22407705c1249a79adee45d8d6
Dmitri Smirnov <dmitrism@microsoft.com>
2024-10-23 10:38:24 -0700

    Resolve the issue with namespace

--- src/csharp/Adapters.cs
+++ src/csharp/Adapters.cs
@@ -1,66 +1,68 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.ML.OnnxRuntimeGenAI;
 using System;
 using System.Runtime.InteropServices;
 
-/// <summary>
-/// A container of adapters.
-/// </summary>
-public class Adapters : SafeHandle
+namespace Microsoft.ML.OnnxRuntimeGenAI
 {
-    private Adapters(IntPtr handle) :
-        base(handle, true)
-    {
-    }
-
     /// <summary>
-    /// Creates a container for adapters
-    /// used to load, unload and hold them.
-    /// Throws on error.
+    /// A container of adapters.
     /// </summary>
-    /// <param name="model">Reference to a loaded model</param>
-    /// <returns>new Adapters object</returns>
-    public static Adapters Create(Model model)
+    public class Adapters : SafeHandle
     {
-        Result.VerifySuccess(NativeMethods.OgaCreateAdapters(model.Handle, out IntPtr handle));
-        return new Adapters(handle);
-    }
+        private Adapters(IntPtr handle) :
+            base(handle, true)
+        {
+        }
 
-    /// <summary>
-    /// Method that loads adapter data and assigns it a nmae that
-    /// it can be referred to. Throws on error.
-    /// </summary>
-    /// <param name="adapterPath">file path to load</param>
-    /// <param name="adapterName">adapter name</param>
-    public void LoadAdapter(string adapterPath, string adapterName)
-    {
-        Result.VerifySuccess(NativeMethods.OgaLoadAdapter(handle, 
-            StringUtils.ToUtf8(adapterPath), StringUtils.ToUtf8(adapterName)));
-    }
+        /// <summary>
+        /// Creates a container for adapters
+        /// used to load, unload and hold them.
+        /// Throws on error.
+        /// </summary>
+        /// <param name="model">Reference to a loaded model</param>
+        /// <returns>new Adapters object</returns>
+        public static Adapters Create(Model model)
+        {
+            Result.VerifySuccess(NativeMethods.OgaCreateAdapters(model.Handle, out IntPtr handle));
+            return new Adapters(handle);
+        }
 
-    /// <summary>
-    /// Unload the adatper that was loaded by the LoadAdapter method.
-    /// Throws on error.
-    /// </summary>
-    /// <param name="adapterName"></param>
-    public void UnloadAdapter(string adapterName)
-    {
-        Result.VerifySuccess(NativeMethods.OgaUnloadAdapter(handle, StringUtils.ToUtf8(adapterName)));
-    }
+        /// <summary>
+        /// Method that loads adapter data and assigns it a nmae that
+        /// it can be referred to. Throws on error.
+        /// </summary>
+        /// <param name="adapterPath">file path to load</param>
+        /// <param name="adapterName">adapter name</param>
+        public void LoadAdapter(string adapterPath, string adapterName)
+        {
+            Result.VerifySuccess(NativeMethods.OgaLoadAdapter(handle,
+                StringUtils.ToUtf8(adapterPath), StringUtils.ToUtf8(adapterName)));
+        }
 
-    internal IntPtr Handle { get { return handle; } }
+        /// <summary>
+        /// Unload the adatper that was loaded by the LoadAdapter method.
+        /// Throws on error.
+        /// </summary>
+        /// <param name="adapterName"></param>
+        public void UnloadAdapter(string adapterName)
+        {
+            Result.VerifySuccess(NativeMethods.OgaUnloadAdapter(handle, StringUtils.ToUtf8(adapterName)));
+        }
 
-    /// <summary>
-    /// Implement SafeHandle override
-    /// </summary>
-    public override bool IsInvalid => handle == IntPtr.Zero;
+        internal IntPtr Handle { get { return handle; } }
 
-    protected override bool ReleaseHandle()
-    {
-        NativeMethods.OgaDestroyAdapters(handle);
-        handle = IntPtr.Zero;
-        return true;
+        /// <summary>
+        /// Implement SafeHandle override
+        /// </summary>
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle()
+        {
+            NativeMethods.OgaDestroyAdapters(handle);
+            handle = IntPtr.Zero;
+            return true;
+        }
     }
 }
