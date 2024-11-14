9e561e08e7dbb1528bbbea7aca79fc6ace98264f
Chester Liu <4710575+skyline75489@users.noreply.github.com>
2024-11-14 13:52:05 +0800

    [skip] Fix

---
+++ examples/csharp/GennyMaui/GennyMaui/Services/IModelProvider.cs
@@ -0,0 +1,15 @@
+﻿using GennyMaui.Models;
+using Microsoft.ML.OnnxRuntimeGenAI;
+
+
+namespace GennyMaui.Services
+{
+    public interface IModelProvider
+    {
+        Model Model { get; }
+
+        Tokenizer Tokenizer { get; }
+
+        ConfigurationModel Configuration { get; }
+    }
+}
