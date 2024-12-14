514edb1d90bfdff45e1bb0a0da965d24f777a199
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2024-12-14 00:50:36 +0000

    Update Python examples

--- examples/python/model-generate.py
+++ examples/python/model-generate.py
@@ -81,7 +81,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description="End-to-end token generation loop example for gen-ai")
     parser.add_argument('-m', '--model_path', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
-    parser.add_argument("-p", "--provider", type=str, required=True, help="Provider to run model")
+    parser.add_argument("-p", "--provider", type=str, required=True, choices=["cpu", "cuda", "dml"], help="Provider to run model")
     parser.add_argument('-pr', '--prompts', nargs='*', required=False, help='Input prompts to generate tokens from. Provide this parameter multiple times to batch multiple prompts')
     parser.add_argument('-i', '--min_length', type=int, default=25, help='Min number of tokens to generate including the prompt')
     parser.add_argument('-l', '--max_length', type=int, default=50, help='Max number of tokens to generate including the prompt')
