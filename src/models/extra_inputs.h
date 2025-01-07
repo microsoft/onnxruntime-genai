bc34b7f1481d25c725d69ae1f33e66873cac5b7a
Baiju Meswani <bmeswani@microsoft.com>
2025-01-07 18:25:33 +0000

    Allow some known extra inputs in the model

--- src/models/extra_inputs.h
+++ src/models/extra_inputs.h
@@ -4,6 +4,18 @@
 
 namespace Generators {
 
+struct PresetExtraInputs {
+  PresetExtraInputs(State& state);
+  void Add();
+
+ private:
+  using FuncType = std::function<std::unique_ptr<OrtValue>()>;
+  State& state_;
+  std::unordered_map<std::string, FuncType> registry_;
+  std::vector<std::unique_ptr<OrtValue>> extra_inputs_;
+  std::vector<std::string> extra_input_names_;
+};
+
 struct ExtraInputs {
   ExtraInputs(State& state);
   void Add();
@@ -14,6 +26,7 @@ struct ExtraInputs {
   std::vector<OrtValue*> extra_inputs_;
   std::vector<std::unique_ptr<OrtValue>> owned_extra_inputs_;
   std::unordered_map<std::string, StaticBuffer*> sb_extra_inputs_;
+  PresetExtraInputs registrar_{state_};
 };
 
 }  // namespace Generators
