853720ea1c8c3bf8f9c3336cfe84d7d59fecfc5c
Abhishek Jindal <abjindal@microsoft.com>
2025-02-06 16:35:02 -0800

    minor fixes

--- examples/c/src/common.h
+++ examples/c/src/common.h
@@ -5,7 +5,7 @@
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
-
+#include <iostream>
 
 using Clock = std::chrono::high_resolution_clock;
 using TimePoint = std::chrono::time_point<Clock>;
--- examples/c/src/cpp/phi3.cpp
+++ examples/c/src/cpp/phi3.cpp
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#pragma once
 #include <iomanip>
-#include <iostream>
 #include <string>
 #include <cstring>
 #include "ort_genai.h"
