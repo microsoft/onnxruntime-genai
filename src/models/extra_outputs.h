947bc0f9d0cbab867b624d4cf1164d63c516e863
Yi Ren <reny@microsoft.com>
2025-02-11 09:38:24 +0800

    fix indents

--- src/models/extra_outputs.h
+++ src/models/extra_outputs.h
@@ -3,18 +3,18 @@
 namespace Generators {
 
 struct ExtraOutputs {
-public:
-    ExtraOutputs(State& state);
-    void Add();
-    void Update();
-    OrtValue* GetOutput(const char* name);
+ public:
+  ExtraOutputs(State& state);
+  void Add();
+  void Update();
+  OrtValue* GetOutput(const char* name);
 
-private:
-    State& state_;
-    // manage output ortvalues not specified in output_names_
-    std::unordered_map<std::string, std::unique_ptr<OrtValue>> output_ortvalue_store_;
-    std::vector<std::string> all_output_names_;  // keep output strings in scope
-    size_t extra_outputs_start_{std::numeric_limits<size_t>::max()};
+ private:
+  State& state_;
+  // manage output ortvalues not specified in output_names_
+  std::unordered_map<std::string, std::unique_ptr<OrtValue>> output_ortvalue_store_;
+  std::vector<std::string> all_output_names_;  // keep output strings in scope
+  size_t extra_outputs_start_{std::numeric_limits<size_t>::max()};
 };
 
 
