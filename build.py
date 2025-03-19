7c7df82d55762a90161a6f698a8ebfc9a3bb8599
Abhishek Jindal <abjindal@microsoft.com>
2025-03-19 11:27:15 -0700

    default true for binskim

--- build.py
+++ build.py
@@ -68,6 +68,7 @@ def _parse_args():
     parser.add_argument(
         "--use_binskim_compliant_compile_flags",
         action="store_true",
+        default=True,
         help="Use compile flags compliant with BinSkim.")
 
     parser.add_argument(
