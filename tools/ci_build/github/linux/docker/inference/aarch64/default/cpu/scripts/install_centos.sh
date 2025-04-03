9662fdd16372aa68bd8d2004abbcd0b88a55b312
Baiju Meswani <bmeswani@microsoft.com>
2025-04-03 09:30:11 -0700

    Update pybind and fix rpath for macos and check for nullptr (#1367)

--- tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
@@ -5,7 +5,6 @@ os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
 
 echo "installing for CentOS version : $os_major_version"
 dnf install -y \
-  python3-devel python3-pip python3-wheel\
   glibc-langpack-\* glibc-locale-source which redhat-lsb-core \
   expat-devel tar unzip zlib-devel make bzip2 bzip2-devel \
   java-11-openjdk-devel graphviz \
