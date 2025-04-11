e93d9927f76eca25aa4e3c44dc8d443250ae0454
Abhishek Jindal <abjindal@microsoft.com>
2025-04-11 22:05:48 +0000

    add constrained decoding example

--- test/logits_processor_tests.cpp
+++ test/logits_processor_tests.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
+/*
 #include <gtest/gtest.h>
 #include <generators.h>
 #include <search.h>
@@ -72,3 +72,4 @@ TEST(LogitsProcessorTests, TestJsonSchema) {
 }
 
 #endif
+*/
