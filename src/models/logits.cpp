2ab145a2f558ae710a9d747feba8398f5550b179
Ryan Metcalfe <107415876+RyanMetcalfeInt8@users.noreply.github.com>
2025-04-18 15:57:00 -0400

    OpenVINO: Model Managed KVCache (#1399)

--- src/models/logits.cpp
+++ src/models/logits.cpp
@@ -3,6 +3,7 @@
 #include "../generators.h"
 #include "model.h"
 #include "logits.h"
+#include "../openvino/interface.h"
 
 namespace Generators {
 
@@ -20,6 +21,17 @@ Logits::Logits(State& state)
   }
 
   input_sequence_lengths.resize(state_.params_->search.batch_size);
+
+  if (IsOpenVINOStatefulModel(state.model_)) {
+    // In the case of OpenVINO stateful models, they are patched in a way so that they only return the
+    // sliced logits needed for sampling. For example, given 43 prompt tokens, instead of returning
+    // logits of the shape:  [1,43,<vocab_size>]
+    // they will have shape: [1, 1,<vocab_size>].
+    if (g_log.enabled)
+      Log("info", "Logits: Using Trimmed Prefill Logits");
+
+    trimmed_prefill_logits_ = true;
+  }
 }
 
 DeviceSpan<float> Logits::Get() {
@@ -91,6 +103,10 @@ DeviceSpan<float> Logits::Get() {
 }
 
 void Logits::Update(const DeviceSpan<int32_t>& next_tokens, size_t new_kv_length) {
+  if (trimmed_prefill_logits_) {
+    new_kv_length = 1;
+  }
+
   if (output_raw_->ort_tensor_ && static_cast<size_t>(output_raw_->GetShape()[1]) == new_kv_length && new_kv_length == 1) {
     return;
   }
--- src/models/model.cpp
+++ src/models/model.cpp
@@ -62,6 +62,17 @@ void State::Run(OrtSession& session, bool graph_capture_this_run) {
     DumpTensors(model_, stream, outputs_.data(), output_names_.data(), output_names_.size(), false);
   }
 
+  if (!ep_dynamic_options_next_run_.empty()) {
+    std::vector<const char*> keys;
+    std::vector<const char*> values;
+    for (auto& kv_pair : ep_dynamic_options_next_run_) {
+      keys.push_back(kv_pair.first.c_str());
+      values.push_back(kv_pair.second.c_str());
+    }
+    session.SetEpDynamicOptions(keys.data(), values.data(), ep_dynamic_options_next_run_.size());
+    ep_dynamic_options_next_run_.clear();
+  }
+
   session.Run(run_options_.get(), input_names_.data(), inputs_.data(), input_names_.size(),
               output_names_.data(), outputs_.data(), output_names_.size());
 
