1e2c846fcbbbf5c5d2fd59e279d07462a2b9f99d
Bhavik Sharda <10757940+BLSharda@users.noreply.github.com>
2025-04-25 10:03:37 +0530

    Update model.cpp
    Fixed merge related compilation error.

--- src/models/model.cpp
+++ src/models/model.cpp
@@ -362,7 +362,7 @@ DeviceInterface* SetProviderSessionOptions(OrtSessionOptions& session_options,
       } else if (provider_options.name == "NvTensorRtRtx") {
         // After setting the NvTensorRtRtx provider in Onnxruntime, GenAI will then treat it as the cuda device.
         session_options.AddConfigEntry("ep.nvtensorrtrtxexecutionprovider.nv_cuda_graph_enable", "1");
-        p_device_ = GetDeviceInterface(DeviceType::CUDA);
+        p_device = GetDeviceInterface(DeviceType::CUDA);
       }
 
       std::vector<const char*> keys, values;
