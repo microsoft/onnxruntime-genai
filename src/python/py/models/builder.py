4c2e1e08b41fdb0b83d4aefd5ef7eed196aad50e
David Fan <30608893+jiafatom@users.noreply.github.com>
2025-04-25 12:07:25 -0700

    Adapt to MatMulNBitsQuantizer in ort (#1426)

--- src/python/py/models/builder.py
+++ src/python/py/models/builder.py
@@ -9,7 +9,14 @@ Run this script to create the desired ONNX model.
 """
 
 from onnx import helper, numpy_helper, TensorProto, external_data_helper, save_model
-from onnxruntime.quantization.matmul_nbits_quantizer import MatMulNBitsQuantizer, QuantFormat
+from onnxruntime import __version__ as ort_version
+from packaging import version
+
+if version.parse(ort_version) > version.parse("1.21.1"):
+    from onnxruntime.quantization.matmul_nbits_quantizer import MatMulNBitsQuantizer, QuantFormat
+else:
+    from onnxruntime.quantization.matmul_4bits_quantizer import MatMul4BitsQuantizer as MatMulNBitsQuantizer, QuantFormat
+
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 import numpy as np
 import torch
