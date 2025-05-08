f8f6e027e8f77eed53ca70fcb87fdc51d17da0ac
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2025-05-08 23:43:46 +0000

    Add more granular control over LayerNorm casts

--- src/python/py/models/builder.py
+++ src/python/py/models/builder.py
@@ -177,7 +177,13 @@ class Model:
             "output_3": "",             # Output 3 for SkipLayerNorm
             "add_offset": 0,            # Offset value for LayerNorm weight
             "epsilon": epsilon,         # Epsilon value to avoid `sqrt(0)` in LayerNorm
-            "use_fp32": False,          # Use float32 precision to compute LayerNorm
+            "cast": {                   # Casting LayerNorm-specific variables
+                "use_fp32": False,      # Use float32 precision to compute LayerNorm
+                "root_input": False,    # Cast root_input
+                "skip_input": False,    # Cast skip_input
+                "output_0": False,      # Cast output_0
+                "output_3": False,      # Cast output_3
+            }
         }
 
         # MatMul-specific variables
@@ -1055,7 +1061,7 @@ class Model:
         # Get precision types to use
         old_torch_dtype = self.to_torch_dtype[self.io_dtype]
         old_io_dtype = self.io_dtype
-        new_torch_dtype = torch.float32 if self.layernorm_attrs["use_fp32"] else self.to_torch_dtype[self.io_dtype]
+        new_torch_dtype = torch.float32 if self.layernorm_attrs["cast"]["use_fp32"] else self.to_torch_dtype[self.io_dtype]
         new_io_dtype = self.to_onnx_dtype[new_torch_dtype]
         cast = old_torch_dtype != new_torch_dtype
 
@@ -1126,14 +1132,15 @@ class Model:
         output_0 = outputs[0]
         output_3 = outputs[3] if skip and not self.layernorm_attrs["last_layernorm"] else None
 
-        # Cast root_input
-        root_input_cast_name = f"{name}/root_input/Cast"
-        root_input_cast_output = f"{root_input_cast_name}/output_0"
-        self.make_node("Cast", inputs=[root_input], outputs=[root_input_cast_output], name=root_input_cast_name, to=new_dtype)
-        self.make_value_info(root_input_cast_output, new_dtype, shape=get_shape_of_value_info(root_input))
-        inputs[0] = root_input_cast_output
+        if self.layernorm_attrs["cast"]["root_input"]:
+            # Cast root_input
+            root_input_cast_name = f"{name}/root_input/Cast"
+            root_input_cast_output = f"{root_input_cast_name}/output_0"
+            self.make_node("Cast", inputs=[root_input], outputs=[root_input_cast_output], name=root_input_cast_name, to=new_dtype)
+            self.make_value_info(root_input_cast_output, new_dtype, shape=get_shape_of_value_info(root_input))
+            inputs[0] = root_input_cast_output
 
-        if skip:
+        if skip and self.layernorm_attrs["cast"]["skip_input"]:
             # Cast skip_input
             skip_input_cast_name = f"{name}/skip_input/Cast"
             skip_input_cast_output = f"{skip_input_cast_name}/output_0"
@@ -1141,14 +1148,15 @@ class Model:
             self.make_value_info(skip_input_cast_output, new_dtype, shape=get_shape_of_value_info(skip_input))
             inputs[1] = skip_input_cast_output
 
-        # Cast output_0
-        output_0_cast_name = f"{name}/output_0/Cast"
-        output_0_cast_output = f"{output_0_cast_name}/output_0"
-        self.make_node("Cast", inputs=[output_0_cast_output], outputs=[output_0], name=output_0_cast_name, to=old_dtype)
-        self.make_value_info(output_0, old_dtype, shape=get_shape_of_value_info(root_input))
-        outputs[0] = output_0_cast_output
+        if self.layernorm_attrs["cast"]["output_0"]:
+            # Cast output_0
+            output_0_cast_name = f"{name}/output_0/Cast"
+            output_0_cast_output = f"{output_0_cast_name}/output_0"
+            self.make_node("Cast", inputs=[output_0_cast_output], outputs=[output_0], name=output_0_cast_name, to=old_dtype)
+            self.make_value_info(output_0, old_dtype, shape=get_shape_of_value_info(root_input))
+            outputs[0] = output_0_cast_output
 
-        if skip and not self.layernorm_attrs["last_layernorm"]:
+        if skip and not self.layernorm_attrs["last_layernorm"] and self.layernorm_attrs["cast"]["output_3"]:
             # Cast output_3
             output_3_cast_name = f"{name}/output_3/Cast"
             output_3_cast_output = f"{output_3_cast_name}/output_3"
@@ -1357,7 +1365,7 @@ class Model:
         layernorm_kwargs = {"epsilon": self.layernorm_attrs["epsilon"], "axis": -1, "stash_type": 1}
         old_torch_dtype = self.to_torch_dtype[self.io_dtype]
         old_io_dtype = self.io_dtype
-        new_torch_dtype = torch.float32 if self.layernorm_attrs["use_fp32"] else self.to_torch_dtype[self.io_dtype]
+        new_torch_dtype = torch.float32 if self.layernorm_attrs["cast"]["use_fp32"] else self.to_torch_dtype[self.io_dtype]
         new_io_dtype = self.to_onnx_dtype[new_torch_dtype]
         cast = old_torch_dtype != new_torch_dtype
 
@@ -2906,7 +2914,11 @@ class GemmaModel(MistralModel):
 class Gemma2Model(GemmaModel):
     def __init__(self, config, io_dtype, onnx_dtype, ep, cache_dir, extra_options):
         super().__init__(config, io_dtype, onnx_dtype, ep, cache_dir, extra_options)
-        self.layernorm_attrs["use_fp32"] = True
+        self.layernorm_attrs["cast"]["use_fp32"] = True
+        self.layernorm_attrs["cast"]["root_input"] = True
+        self.layernorm_attrs["cast"]["skip_input"] = False
+        self.layernorm_attrs["cast"]["output_0"] = True
+        self.layernorm_attrs["cast"]["output_3"] = False
         self.attention_attrs["scale"] = config.query_pre_attn_scalar ** -0.5
         self.is_local = lambda layer_id: layer_id % 2 == 1
 
@@ -2915,30 +2927,59 @@ class Gemma2Model(GemmaModel):
         self.output_types["logits"] = TensorProto.FLOAT
         super().make_outputs_init()
 
+    def make_layernorm(self, layer_id, layernorm, skip, simple, location):
+        if "final_norm" in location:
+            # Set cast for final LayerNorm since it is a special case and not covered in `make_layer`
+            self.layernorm_attrs["cast"]["root_input"] = False
+        super().make_layernorm(layer_id, layernorm, skip, simple, location)
+
     def make_layer(self, layer_id, layer):
         # Gemma2 decoder layer is typically defined as:
         # input_layernorm --> attention --> post_attention_layernorm --> pre_ffn_layernorm --> MLP --> post_ffn_layernorm
+
+        # Adjust LayerNorm attributes because of extra LayerNorms inserted
+        # 1. Only cast root_input if the first layer of LayerNorms are being created
+        original_cast_root_input = self.layernorm_attrs["cast"]["root_input"]
+        self.layernorm_attrs["cast"]["root_input"] = self.layernorm_attrs["first_layernorm"]
         self.make_layernorm(layer_id, layer.input_layernorm, skip=not self.layernorm_attrs["first_layernorm"], simple=self.layernorm_attrs["simple"], location="input")
+        self.layernorm_attrs["cast"]["root_input"] = original_cast_root_input
+
         self.make_attention(layer_id, layer.self_attn, root_input=self.layernorm_attrs["output_0"])
 
-        # Temporarily set root_input for LayerNorm to skip_input for post_attention_layernorm
-        # Set skip_input to output of post_attention_layernorm
+        # Adjust LayerNorm attributes for extra LayerNorm to insert
+        # 1. Temporarily set root_input for LayerNorm to skip_input for post_attention_layernorm
+        # 2. Set skip_input to output of post_attention_layernorm
+        # 3. Do not cast outputs from post_attention_layernorm
         original_root_input = self.layernorm_attrs["root_input"]
+        original_cast_output_0 = self.layernorm_attrs["cast"]["output_0"]
         self.layernorm_attrs["root_input"] = self.layernorm_attrs["skip_input"]
+        self.layernorm_attrs["cast"]["output_0"] = False
         self.make_layernorm(layer_id, layer.post_attention_layernorm, skip=False, simple=self.layernorm_attrs["simple"], location="post_attention")
         self.layernorm_attrs["root_input"] = original_root_input
         self.layernorm_attrs["skip_input"] = self.layernorm_attrs["output_0"]
+        self.layernorm_attrs["cast"]["output_0"] = original_cast_output_0
 
+        # Adjust LayerNorm attributes because of extra LayerNorms inserted
+        # 1. Only cast root_input if the first layer of LayerNorms are being created
+        original_cast_root_input = self.layernorm_attrs["cast"]["root_input"]
+        self.layernorm_attrs["cast"]["root_input"] = self.layernorm_attrs["first_layernorm"]
         self.make_layernorm(layer_id, layer.pre_feedforward_layernorm, skip=True, simple=self.layernorm_attrs["simple"], location="pre_feedforward")
+        self.layernorm_attrs["cast"]["root_input"] = original_cast_root_input
+
         self.make_mlp(layer_id, layer.mlp, root_input=self.layernorm_attrs["output_0"])
 
-        # Temporarily set root_input for LayerNorm to skip_input for post_feedforward_layernorm
-        # Set skip_input to output of post_ffn_layernorm
+        # Adjust LayerNorm attributes for extra LayerNorm to insert
+        # 1. Temporarily set root_input for LayerNorm to skip_input for post_feedforward_layernorm
+        # 2. Set skip_input to output of post_feedforward_layernorm
+        # 3. Do not cast outputs from post_feedforward_layernorm
         original_root_input = self.layernorm_attrs["root_input"]
+        original_cast_output_0 = self.layernorm_attrs["cast"]["output_0"]
         self.layernorm_attrs["root_input"] = self.layernorm_attrs["skip_input"]
+        self.layernorm_attrs["cast"]["output_0"] = False
         self.make_layernorm(layer_id, layer.post_feedforward_layernorm, skip=False, simple=self.layernorm_attrs["simple"], location="post_feedforward")
         self.layernorm_attrs["root_input"] = original_root_input
         self.layernorm_attrs["skip_input"] = self.layernorm_attrs["output_0"]
+        self.layernorm_attrs["cast"]["output_0"] = original_cast_output_0
 
         self.layernorm_attrs["first_layernorm"] = False
         if layer_id == self.num_layers - 1:
