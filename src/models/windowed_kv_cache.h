1620ee3e5fe38102bb79c752279c1e6fd1341592
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-05-29 17:28:59 -0700

    save work - initial PartialUpdate impl

--- src/models/windowed_kv_cache.h
+++ src/models/windowed_kv_cache.h
@@ -20,37 +20,55 @@ struct WindowedKeyValueCache : KeyValueCache {
   bool IsPartialUpdateSupported() const override { return true; }
 
   void PartialUpdate(DeviceSpan<int32_t> beam_indices, int total_length,
-                                    std::span<const size_t> layer_indices_to_update) override;
+                     std::span<const size_t> layer_indices_to_update) override;
 
   void RewindTo(size_t index) override {
     throw std::runtime_error("WindowedKeyValueCache does not support RewindTo.");
   }
 
  private:
+  using CacheTensorShape = std::array<int64_t, 4>;
+  struct LayerState;
+
+  static std::vector<LayerState> MakeInitialPerLayerStates(size_t layer_count,
+                                                           size_t initial_window_size,
+                                                           const CacheTensorShape& initial_key_cache_shape_in,
+                                                           const CacheTensorShape& initial_key_cache_shape_out,
+                                                           const CacheTensorShape& initial_value_cache_shape_in,
+                                                           const CacheTensorShape& initial_value_cache_shape_out);
+
   void SlideLayer(size_t layer_idx);
-  void SlideAllLayers();
-  void SlideLayers(std::span<const size_t> layer_indices);
+  void TransitionLayerToTokenGeneration(size_t layer_idx);
+  void UpdateLayer(DeviceSpan<int32_t> beam_indices, int total_length, size_t layer_idx);
 
   DeviceInterface& Device() { return *model_.p_device_kvcache_; }
   Ort::Allocator& Allocator() { return model_.p_device_kvcache_->GetAllocator(); }
 
   State& state_;
   const Model& model_{state_.model_};
-  int layer_count_{};
-  int window_size_{};
-  size_t num_windows_{};
-  size_t window_index_{};
+  const size_t layer_count_;
+
+  struct LayerState {
+    size_t window_index{0};
+    size_t window_size{};
+    size_t num_windows{};
+    bool is_first_update{true};
+
+    CacheTensorShape key_cache_shape_in, key_cache_shape_out;
+    CacheTensorShape value_cache_shape_in, value_cache_shape_out;
+  };
+
+  std::vector<LayerState> per_layer_states_;
+
   size_t input_index_{~0U}, output_index_{~0U};
 
-  std::array<int64_t, 4> key_cache_shape_in_, key_cache_shape_out_;
-  std::array<int64_t, 4> value_cache_shape_in_, value_cache_shape_out_;
   ONNXTensorElementDataType type_;
 
   std::vector<std::unique_ptr<OrtValue>> key_caches_in_, value_caches_in_;
   std::vector<std::unique_ptr<OrtValue>> key_caches_out_, value_caches_out_;
   std::vector<std::string> input_name_strings_, output_name_strings_;
 
-  bool is_first_update_{true};
+  const std::vector<size_t> all_layer_indices_;
 };
 
 }  // namespace Generators
