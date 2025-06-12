1bd3c22c85d51bf69f83faea5fe3ed9b1a6c22b7
Justin Chu <justinchuby@users.noreply.github.com>
2025-06-12 16:15:25 -0700

    Fix merge conflicts

--- src/python/py/models/builder.py
+++ src/python/py/models/builder.py
@@ -1518,7 +1518,7 @@ class Model:
         self.make_cast(make_cast_name, root_input, ir.DataType.FLOAT, shape=shape)
 
         make_pow_name = f"{basename}/Pow"
-        make_pow_inputs = [f"{make_cast_name}/output_0", f"/model/constants/FLOAT/0D/2"]
+        make_pow_inputs = [f"{make_cast_name}/output_0", "/model/constants/FLOAT/0D/2"]
 
         self.make_node("Pow", inputs=make_pow_inputs, outputs=[f"{make_pow_name}/output_0"], name=make_pow_name, domain="")
         self.make_value_info(f"{make_pow_name}/output_0", ir.DataType.FLOAT, shape=shape)
@@ -1529,15 +1529,15 @@ class Model:
 
         make_add_name = f"{basename}/Add"
         make_add_inputs = [f"{make_reducemean_name}/output_0", f"/model/constants/FLOAT/0D/{self.layernorm_attrs['epsilon']}"]
-        self.make_add(make_add_name, make_add_inputs, TensorProto.FLOAT, shape=shape)
+        self.make_add(make_add_name, make_add_inputs, ir.DataType.FLOAT, shape=shape)
 
         make_sqrt_name = f"{basename}/Sqrt"
         make_sqrt_inputs = [f"{make_add_name}/output_0"]
         self.make_sqrt(make_sqrt_name, make_sqrt_inputs, ir.DataType.FLOAT, shape=shape)
 
         make_div_name = f"{basename}/Div"
-        make_div_inputs = [f"/model/constants/FLOAT/0D/1", f"{make_sqrt_name}/output_0"]
-        self.make_div(make_div_name, make_div_inputs, TensorProto.FLOAT, shape=shape)
+        make_div_inputs = ["/model/constants/FLOAT/0D/1", f"{make_sqrt_name}/output_0"]
+        self.make_div(make_div_name, make_div_inputs, ir.DataType.FLOAT, shape=shape)
 
         make_mul_name = f"{basename}/Mul"
         make_mul_inputs = [f"{make_div_name}/output_0", f"{make_cast_name}/output_0"]
