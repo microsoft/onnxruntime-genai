62bd95ad5c0c603af2d0f66eaca895125ffcbd9d
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2025-06-29 15:45:31 +0000

    Add another missing import

--- test/python/test_onnxruntime_genai_e2e.py
+++ test/python/test_onnxruntime_genai_e2e.py
@@ -9,7 +9,7 @@ import logging
 import sys
 
 import onnxruntime_genai as og
-from _test_utils import get_ci_data_path
+from _test_utils import get_ci_data_path, run_subprocess
 
 logging.basicConfig(
     format="%(asctime)s %(name)s [%(levelname)s] - %(message)s", level=logging.DEBUG
