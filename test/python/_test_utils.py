d2bb51d6656ebc2b9b0c58bbb495ab8a5f1c86d4
Jambay Kinley <jambaykinley@microsoft.com>
2025-07-28 22:04:55 +0000

    comment out mistral

--- test/python/_test_utils.py
+++ test/python/_test_utils.py
@@ -81,7 +81,7 @@ def get_model_paths():
     ci_paths = {
         "llama-2": os.path.join(ci_data_path, "Llama-2-7B-Chat-GPTQ"),
         "llama-3": os.path.join(ci_data_path, "Meta-Llama-3-8B-AWQ"),
-        "mistral-v0.2": os.path.join(ci_data_path, "Mistral-7B-Instruct-v0.2-GPTQ"),
+        # "mistral-v0.2": os.path.join(ci_data_path, "Mistral-7B-Instruct-v0.2-GPTQ"),
         "phi-2": os.path.join(ci_data_path, "phi2"),
         # "gemma-2b": os.path.join(ci_data_path, "gemma-1.1-2b-it"),
         "gemma-7b": os.path.join(ci_data_path, "gemma-7b-it-awq"),
