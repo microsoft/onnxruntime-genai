724a21621a21d732cf03a0b5d8cb9752d5d95e10
Anuj Jalota <ajalota@nvidia.com>
2025-09-07 07:21:00 +0530

    Add disable_synchronize_execution_providers for NV EP

--- src/models/model.cpp
+++ src/models/model.cpp
@@ -144,6 +144,10 @@ void State::Run(OrtSession& session, bool graph_capture_this_run) {
     ep_dynamic_options_next_run_.clear();
   }
 
+  if (model_.p_device_ && model_.p_device_->GetType() == DeviceType::NvTensorRtRtx) {
+    run_options_->AddConfigEntry("disable_synchronize_execution_providers", "1");
+  }
+
   session.Run(run_options_.get(), input_names_.data(), inputs_.data(), input_names_.size(),
               output_names_.data(), outputs_.data(), output_names_.size());
 
@@ -591,17 +595,16 @@ DeviceInterface* SetProviderSessionOptions(OrtSessionOptions& session_options,
       }
 
       std::vector<const char*> keys, values;
-      std::vector<std::string> extra_values;  // To hold the string values in scope
 
-      // For NvTensorRtRtx, add user_compute_stream to provider options if we have a device
+      std::string stream_value_str;
       if (provider_options.name == "NvTensorRtRtx" && is_primary_session_options && p_device) {
         void* stream_ptr = p_device->GetCudaStream();
         std::stringstream stream_value;
         stream_value << reinterpret_cast<uintptr_t>(stream_ptr);
-        extra_values.push_back(stream_value.str());
+        stream_value_str = stream_value.str();
 
         keys.emplace_back("user_compute_stream");
-        values.emplace_back(extra_values.back().c_str());
+        values.emplace_back(stream_value_str.c_str());
       }
 
       for (auto& option : provider_options.options) {
