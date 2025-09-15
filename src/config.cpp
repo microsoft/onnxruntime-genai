microsoft/onnxruntime-genai

commit 149920309618ff66487f2d3f62aeccbca396f747

Hello WindowsML (#1711)
# Hello Windows ML!

Adding GenAI support for the Windows ML (WinML) ORT based distribution.

## ✅Behavior Changes:
- `SetProviderSessionOptions` now calls
`SessionOptionsAppendExecutionProvider_V2` when building the Execution
Providers to leverage the new Plugin EP pattern.
 
## 🎯New Build Targets:

WinML builds for two targets, x64 and arm64. In both case we define
`USE_WINML` along with the following:
- `windows_x64_winml_relwithdebinfo`
  - `USE_DML`, `USE_CUDA`
- `windows_arm64_winml_relwithdebinfo`
  - `USE_DML`

When building it is REQURED to specify the version of the
`Microsoft.WindowsAppSDK.ML` we are building against in the
`WINML_SDK_VERSION` variable.

### Example:

```bash
make --preset windows_arm64_winml_relwithdebinfo -T cuda='E:\_work\1\onnxruntime-genai\cuda_sdk\v12.2'

make --preset windows_x64_winml_relwithdebinfo -T cuda='E:\_work\1\onnxruntime-genai\cuda_sdk\v12.2' -DWINML_SDK_VERSION='1.8.1075-preview-g295f112894'
```

## 📋Testing
- Don't break existing build targets, added `win-winml-x64-build.yml`
for CI.

## 🤔Open Questions:
- Seems like the decision between
`SessionOptionsAppendExecutionProvider` and
`SessionOptionsAppendExecutionProvider_V2` is not specific to WinML but
the new Plugin EP pattern on ORT.
- Should the WINML and V2 concepts be split out? Does non-winml GenAI
code want to use the V2 pattern?

## 🪫Batteries not included (debt):
  - Add targeted WinML tests.

---------

Co-authored-by: Yi Ren <reny@microsoft.com>

src/config.cpp
@@ -14,12 +14,20 @@ namespace Generators {
 
 // Fix casing of certain historical names to match current Onnxruntime names
 std::string_view NormalizeProviderName(std::string_view name) {
-  if (name == "qnn") {
+  std::string lower_name(name);
+  std::transform(lower_name.begin(), lower_name.end(), lower_name.begin(), [](unsigned char c) { return static_cast<unsigned char>(std::tolower(c)); });
+  if (lower_name == "qnn") {
     return "QNN";
-  } else if (name == "webgpu") {
+  } else if (lower_name == "webgpu") {
     return "WebGPU";
-  } else if (name == "dml") {
+  } else if (lower_name == "dml") {
     return "DML";
+  } else if (lower_name == "openvino") {
+    return "OpenVINO";
+  } else if (lower_name == "vitisai") {
+    return "VitisAI";
+  } else if (lower_name == "nvtensorrtrtx") {
+    return "NvTensorRTRTX";
   }
   return name;  // Return name unchanged
 }
@@ -709,6 +717,12 @@ struct Model_Element : JSON::Element {
       v_.decoder_start_token_id = static_cast<int>(JSON::Get<double>(value));
     } else if (name == "sep_token_id") {
       v_.sep_token_id = static_cast<int>(JSON::Get<double>(value));
+    } else if (name == "hardware_device_type") {
+      v_.hardware_device_type = JSON::Get<std::string_view>(value);
+    } else if (name == "hardware_device_id") {
+      v_.hardware_device_id = static_cast<uint32_t>(JSON::Get<double>(value));
+    } else if (name == "hardware_vendor_id") {
+      v_.hardware_vendor_id = static_cast<uint32_t>(JSON::Get<double>(value));
     } else {
       throw JSON::unknown_value_error{};
     }

src/models/model.cpp
@@ -593,6 +593,135 @@ DeviceInterface* SetProviderSessionOptions(OrtSessionOptions& session_options,
         p_device = GetDeviceInterface(DeviceType::NvTensorRtRtx);
       }
 
+#if USE_WINML
+      // Get model device config
+      std::optional<uint32_t> config_device_id = config.model.hardware_device_id;
+      std::optional<uint32_t> config_vendor_id = config.model.hardware_vendor_id;
+      std::optional<std::string> config_device_type = config.model.hardware_device_type;
+      // for OpenVINO, use "device_type" in provider_options exclusively if it's provided
+      std::optional<std::string> config_ov_device_type = std::nullopt;
+      if (provider_options.name == "OpenVINO") {
+        for (auto& option : provider_options.options) {
+          if (option.first == "device_type") {
+            config_ov_device_type = option.second;
+          }
+        }
+        if (config_ov_device_type.has_value()) {
+          config_device_id = std::nullopt;
+          config_vendor_id = std::nullopt;
+          config_device_type = std::nullopt;
+        } else if (!(config_device_id.has_value() || config_vendor_id.has_value() || config_device_type.has_value())) {
+          config_ov_device_type = "CPU";
+        }
+      }
+      std::optional<OrtHardwareDeviceType> config_device_type_enum;
+      if (config_device_type.has_value()) {
+        if (*config_device_type == "CPU") {
+          config_device_type_enum = OrtHardwareDeviceType_CPU;
+        } else if (*config_device_type == "GPU") {
+          config_device_type_enum = OrtHardwareDeviceType_GPU;
+        } else if (*config_device_type == "NPU") {
+          config_device_type_enum = OrtHardwareDeviceType_NPU;
+        } else {
+          throw std::runtime_error("Unsupported hardware device type: " + *config_device_type);
+        }
+      }
+
+      // Match EP device with EP name in provider options and model device config
+      // include\onnxruntime\core\graph\constants.h
+      const static std::unordered_map<std::string, std::string> s_providerNameToExecutionProvider{
+          {"QNN", "QNNExecutionProvider"},
+          {"WebGPU", "WebGpuExecutionProvider"},
+          {"OpenVINO", "OpenVINOExecutionProvider"},
+          {"VitisAI", "VitisAIExecutionProvider"},
+          {"NvTensorRtRtx", "NvTensorRTRTXExecutionProvider"},
+      };
+      std::string ep_name{};
+      if (auto search = s_providerNameToExecutionProvider.find(provider_options.name); search != s_providerNameToExecutionProvider.end()) {
+        ep_name = search->second;
+      }
+
+      size_t num_devices = 0;
+      const OrtEpDevice* const* device_ptrs = nullptr;
+      Ort::GetEpDevices(&GetOrtEnv(), &device_ptrs, &num_devices);
+
+      std::vector<const OrtEpDevice*> ep_devices_ptrs;
+      ep_devices_ptrs.reserve(num_devices);
+
+      for (size_t i = 0; i < num_devices; ++i) {
+        const OrtHardwareDevice* hardware_device = Ort::api->EpDevice_Device(device_ptrs[i]);
+        const uint32_t hardware_device_id = Ort::api->HardwareDevice_DeviceId(hardware_device);
+        const uint32_t hardware_vendor_id = Ort::api->HardwareDevice_VendorId(hardware_device);
+        const OrtHardwareDeviceType hardware_device_type = Ort::api->HardwareDevice_Type(hardware_device);
+
+        auto check_ov_device_type = [&config_ov_device_type, &provider_options](const OrtEpDevice* device_ptr) -> bool {
+          if (provider_options.name != "OpenVINO") {
+            return true;
+          } else if (!config_ov_device_type.has_value()) {
+            return true;
+          } else {
+            const OrtKeyValuePairs* keyvals = Ort::api->EpDevice_EpMetadata(device_ptr);
+            size_t num_entries;
+            const char* const* keys = nullptr;
+            const char* const* values = nullptr;
+            Ort::api->GetKeyValuePairs(keyvals, &keys, &values, &num_entries);
+            for (int kvi = 0; kvi < num_entries; kvi++) {
+              const std::string key = keys[kvi];
+              const std::string val = values[kvi];
+              if (key == "ov_device" && val == config_ov_device_type) {
+                return true;
+              }
+            }
+            return false;
+          }
+        };
+        bool hardware_device_id_matched = (!config_device_id.has_value()) || config_device_id.value() == hardware_device_id;
+        bool hardware_vendor_id_matched = (!config_vendor_id.has_value()) || config_vendor_id.value() == hardware_vendor_id;
+        bool hardware_device_type_matched = (!config_device_type_enum.has_value()) ||
+                                            config_device_type_enum.value() == hardware_device_type;
+        bool hardware_ov_device_type_matched = check_ov_device_type(device_ptrs[i]);
+
+        // Append matched EP device
+        if (Ort::api->EpDevice_EpName(device_ptrs[i]) == ep_name &&
+            hardware_device_id_matched &&
+            hardware_vendor_id_matched &&
+            hardware_device_type_matched &&
+            hardware_ov_device_type_matched) {
+          ep_devices_ptrs.push_back(device_ptrs[i]);
+          // WinML Hotfix: DML and WebGPU EP factories currently only support one device at a time
+          // OpenVINO also supports only one device at a time
+          if (provider_options.name == "DML" || provider_options.name == "WebGPU" || provider_options.name == "OpenVINO") {
+            break;
+          }
+        }
+      }
+
+      // No need to append if we can't find a device.
+      if (!ep_devices_ptrs.empty()) {
+        std::vector<const char*> keys, values;
+        for (auto& option : provider_options.options) {
+          // WinML Hotfix: remove backend_type and backend_path from QNN provider options
+          static const std::set<std::string> qnn_options_to_remove{"backend_type", "backend_path"};
+          if (provider_options.name == "QNN" &&
+              qnn_options_to_remove.find(option.first) != qnn_options_to_remove.end()) {
+            continue;
+          }
+
+          // 'device_type' is not a supported option for OpenVINO when SessionOptionsAppendExecutionProvider_V2 is used.
+          if (provider_options.name == "OpenVINO" && option.first == "device_type") {
+            continue;
+          }
+          keys.emplace_back(option.first.c_str());
+          values.emplace_back(option.second.c_str());
+        }
+
+        Ort::api->SessionOptionsAppendExecutionProvider_V2(
+            &session_options,
+            &GetOrtEnv(),
+            ep_devices_ptrs.data(), ep_devices_ptrs.size(),
+            keys.data(), values.data(), keys.size());
+      }
+#else
       std::vector<const char*> keys, values;
       std::string stream_value_str;
       if (provider_options.name == "NvTensorRtRtx" && is_primary_session_options && p_device) {
@@ -610,6 +739,8 @@ DeviceInterface* SetProviderSessionOptions(OrtSessionOptions& session_options,
         values.emplace_back(option.second.c_str());
       }
       session_options.AppendExecutionProvider(provider_options.name.c_str(), keys.data(), values.data(), keys.size());
+
+#endif
     }
   }
   return p_device;

.github/workflows/win-winml-x64-build.yml
@@ -1,1 +1,70 @@
 name: "Windows WinML x64 Build"
+on:
+  workflow_dispatch:
+  push:
+    branches:
+      - main
+      - rel-*
+  pull_request:
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.event_name == 'pull_request' && github.ref || github.sha }}
+  cancel-in-progress: true
+
+env:
+  AZCOPY_AUTO_LOGIN_TYPE: MSI
+  AZCOPY_MSI_CLIENT_ID: 63b63039-6328-442f-954b-5a64d124e5b4
+  cuda_dir: "${{ github.workspace }}\\cuda_sdk"
+  cuda_version: "12.2"
+  CUDA_PATH: ${{ github.workspace }}\\cuda_sdk\\v12.2
+  binaryDir: 'build/cuda/win-x64'
+  ORT_NIGHTLY_REST_API: "https://feeds.dev.azure.com/aiinfra/PublicPackages/_apis/packaging/Feeds/ORT-Nightly/packages?packageNameQuery=Microsoft.ML.OnnxRuntime.Gpu.Windows&api-version=6.0-preview.1"
+  ORT_PACKAGE_NAME: "Microsoft.ML.OnnxRuntime.Gpu.Windows"
+
+jobs:
+  windows-cuda-x64-build:
+    runs-on: ["self-hosted", "1ES.Pool=onnxruntime-genai-Win2022-GPU-A10"]
+    steps:
+      - name: Checkout OnnxRuntime GenAI repo
+        uses: actions/checkout@v4
+        with:
+          submodules: true
+
+      - uses: actions/setup-python@v5
+        with:
+          python-version: '3.11.x'
+          architecture: 'x64'
+
+      - name: Setup VCPKG
+        uses: microsoft/onnxruntime-github-actions/setup-build-tools@v0.0.6
+        with:
+          vcpkg-version: '2025.03.19'
+          vcpkg-hash: '17e96169cd3f266c4716fcdc1bb728e6a64f103941ece463a2834d50694eba4fb48f30135503fd466402afa139abc847ef630733c442595d1c34979f261b0114'
+          cmake-version: '3.31.6'
+          cmake-hash: '0f1584e8666cf4a65ec514bd02afe281caabf1d45d2c963f3151c41484f457386aa03273ab25776a670be02725354ce0b46f3a5121857416da37366342a833a0'
+          add-cmake-to-path: 'true'
+          disable-terrapin: 'false'
+
+      - name: Download cuda
+        run: |
+          azcopy.exe cp --recursive "https://lotusscus.blob.core.windows.net/models/cuda_sdk/v${{ env.cuda_version }}" ${{ env.cuda_dir}}
+
+      - uses: actions/setup-dotnet@v4
+        with:
+          dotnet-version: '8.0.x'
+
+      - name: Install Rust Toolchain
+        run: |
+          $exePath = "$env:TEMP\rustup-init.exe"
+          (New-Object Net.WebClient).DownloadFile('https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe', $exePath)
+          & $exePath -y --default-toolchain=1.86.0
+          Add-Content $env:GITHUB_PATH "$env:USERPROFILE\.cargo\bin"
+
+      - name: Configure CMake
+        run: |
+          cmake --preset windows_x64_winml_relwithdebinfo -T cuda=${{ env.cuda_dir }}\\v${{ env.cuda_version }} -DWINML_SDK_VERSION='1.8.2088'
+
+      - name: Build with CMake
+        run: |
+          cmake --build --preset windows_x64_winml_relwithdebinfo --parallel
+          cmake --build --preset windows_x64_winml_relwithdebinfo --target PyPackageBuild

.pipelines/nuget-publishing.yml
@@ -1,109 +1,118 @@
 parameters:
-- name: enable_post_packaging_validation
-  displayName: Whether Post Packaging validation is run
-  type: boolean
-  default: false
-- name: enable_win_cpu
-  displayName: 'Whether Windows CPU package is built.'
-  type: boolean
-  default: true
+  - name: enable_post_packaging_validation
+    displayName: Whether Post Packaging validation is run
+    type: boolean
+    default: false
+  - name: enable_win_cpu
+    displayName: 'Whether Windows CPU package is built.'
+    type: boolean
+    default: true
 
-- name: enable_win_cuda
-  displayName: 'Whether Windows CUDA package is built.'
-  type: boolean
-  default: true
+  - name: enable_win_cuda
+    displayName: 'Whether Windows CUDA package is built.'
+    type: boolean
+    default: true
 
-- name: enable_linux_cpu
-  displayName: 'Whether Linux CPU package is built.'
-  type: boolean
-  default: true
+  - name: enable_linux_cpu
+    displayName: 'Whether Linux CPU package is built.'
+    type: boolean
+    default: true
 
-- name: enable_linux_cuda
-  displayName: 'Whether Linux CUDA package is built.'
-  type: boolean
-  default: true
+  - name: enable_linux_cuda
+    displayName: 'Whether Linux CUDA package is built.'
+    type: boolean
+    default: true
 
-- name: enable_win_dml
-  displayName: 'Whether Windows DirectML package is built.'
-  type: boolean
-  default: true
+  - name: enable_win_dml
+    displayName: 'Whether Windows DirectML package is built.'
+    type: boolean
+    default: true
 
-- name: enable_win_arm64
-  displayName: 'Whether Windows ARM64 package is built.'
-  type: boolean
-  default: true
+  - name: enable_win_arm64
+    displayName: 'Whether Windows ARM64 package is built.'
+    type: boolean
+    default: true
 
-- name: enable_macos_cpu
-  displayName: 'Whether MacOS CPU package is built.'
-  type: boolean
-  default: true
+  - name: enable_macos_cpu
+    displayName: 'Whether MacOS CPU package is built.'
+    type: boolean
+    default: true
 
-- name: enable_android
-  displayName: 'Whether Android AAR package is built.'
-  type: boolean
-  default: true
+  - name: enable_android
+    displayName: 'Whether Android AAR package is built.'
+    type: boolean
+    default: true
 
-- name: enable_apple_framework
-  displayName: 'Whether Apple framework for iOS & MacCatalyst is built.'
-  type: boolean
-  default: true
+  - name: enable_apple_framework
+    displayName: 'Whether Apple framework for iOS & MacCatalyst is built.'
+    type: boolean
+    default: true
 
-- name: enable_win_qnn
-  displayName: 'Whether QNN nuget package should be built.'
-  type: boolean
-  default: true
+  - name: enable_win_qnn
+    displayName: 'Whether QNN nuget package should be built.'
+    type: boolean
+    default: true
 
-- name: ort_version
-  displayName: 'OnnxRuntime version'
-  type: string
-  default: '1.22.0'
+  - name: enable_winml
+    displayName: 'Whether Windows WinML package is built.'
+    type: boolean
+    default: false
 
-- name: ort_cuda_version
-  displayName: 'OnnxRuntime GPU version'
-  type: string
-  default: '1.22.0'
+  - name: ort_version
+    displayName: 'OnnxRuntime version'
+    type: string
+    default: '1.22.0'
 
-- name: ort_dml_version
-  displayName: 'OnnxRuntime DML version'
-  type: string
-  default: '1.22.0'
+  - name: ort_winml_version
+    displayName: 'Microsoft.WindowsAppSDK.ML Version (should match CMakeList.txt)'
+    type: string
+    default: '1.8.2088'
 
-- name: cuda_version
-  displayName: 'CUDA version'
-  type: string
-  values:
-  - '11.8'
-  - '12.2'
-  default: '12.2'
+  - name: ort_cuda_version
+    displayName: 'OnnxRuntime GPU version'
+    type: string
+    default: '1.22.0'
 
-- name: build_config
-  displayName: 'Build Configuration'
-  type: string
-  default: 'release'
-  values:
-  - 'release'
-  - 'relwithdebinfo'
-  - 'debug'
+  - name: ort_dml_version
+    displayName: 'OnnxRuntime DML version'
+    type: string
+    default: '1.22.0'
 
-- name: 'nuget_version_type'
-  displayName: 'Nuget version - stable uses VERSION_INFO, dev appends date, build_id and commit-ish, e.g. 1.21.0-dev-20250205-1932-8f6ddf3bd5'
-  type: string
-  default: dev
-  values:
-  - dev
-  - stable
+  - name: cuda_version
+    displayName: 'CUDA version'
+    type: string
+    values:
+      - '11.8'
+      - '12.2'
+    default: '12.2'
 
-- name: custom_ort_package_versioned_name
-  displayName: 'Versioned name of ort nuget package. Must follow Microsoft.ML.OnnxRuntime.*.version.nupkg. Keep None to disable building'
-  type: string
-  default: 'Microsoft.ML.OnnxRuntime.Foundry.1.23.0-dev-20250617-0934-76952334d2.nupkg'
+  - name: build_config
+    displayName: 'Build Configuration'
+    type: string
+    default: 'relwithdebinfo'
+    values:
+      - 'relwithdebinfo'
+      - 'debug'
+
+  - name: 'nuget_version_type'
+    displayName: 'Nuget version - stable uses VERSION_INFO, dev appends date, build_id and commit-ish, e.g. 1.21.0-dev-20250205-1932-8f6ddf3bd5'
+    type: string
+    default: dev
+    values:
+      - dev
+      - stable
+
+  - name: custom_ort_package_versioned_name
+    displayName: 'Versioned name of ort nuget package. Must follow Microsoft.ML.OnnxRuntime.*.version.nupkg. Keep None to disable building'
+    type: string
+    default: 'Microsoft.ML.OnnxRuntime.Foundry.1.23.0-dev-20250617-0934-76952334d2.nupkg'
 
 resources:
   repositories:
-  - repository: 1esPipelines
-    type: git
-    name: 1ESPipelineTemplates/1ESPipelineTemplates
-    ref: refs/tags/release
+    - repository: 1esPipelines
+      type: git
+      name: 1ESPipelineTemplates/1ESPipelineTemplates
+      ref: refs/tags/release
 
 trigger: none
 extends:
@@ -114,8 +123,8 @@ extends:
   parameters:
     # Update the pool with your team's 1ES hosted pool.
     pool:
-      name: 'onnxruntime-Win-CPU-2022'  # Name of your hosted pool
-      os: windows  # OS of the image. This value cannot be a variable. Allowed values: windows, linux, macOS
+      name: 'onnxruntime-Win-CPU-2022' # Name of your hosted pool
+      os: windows # OS of the image. This value cannot be a variable. Allowed values: windows, linux, macOS
     sdl:
       policheck:
         enabled: true
@@ -125,43 +134,7 @@ extends:
         enabled: true
         configFile: '$(Build.SourcesDirectory)\.config\tsaoptions.json'
     stages:
-    - template: stages/capi-packaging-stage.yml
-      parameters:
-        enable_win_cpu: ${{ parameters.enable_win_cpu }}
-        enable_win_cuda: ${{ parameters.enable_win_cuda }}
-        enable_linux_cpu: ${{ parameters.enable_linux_cpu }}
-        enable_linux_cuda: ${{ parameters.enable_linux_cuda }}
-        enable_win_dml: ${{ parameters.enable_win_dml }}
-        enable_win_arm64: ${{ parameters.enable_win_arm64 }}
-        enable_macos_cpu: ${{ parameters.enable_macos_cpu }}
-        enable_android: ${{ parameters.enable_android }}
-        enable_apple_framework: ${{ parameters.enable_apple_framework }}
-        ort_version: ${{ parameters.ort_version }}
-        ort_cuda_version: ${{ parameters.ort_cuda_version }}
-        ort_dml_version: ${{ parameters.ort_dml_version }}
-        cuda_version: ${{ parameters.cuda_version }}
-        build_config: ${{ parameters.build_config }}
-
-    - template: stages/nuget-packaging-stage.yml
-      parameters:
-        enable_win_cpu: ${{ parameters.enable_win_cpu }}
-        enable_win_cuda: ${{ parameters.enable_win_cuda }}
-        enable_linux_cpu: ${{ parameters.enable_linux_cpu }}
-        enable_linux_cuda: ${{ parameters.enable_linux_cuda }}
-        enable_win_dml: ${{ parameters.enable_win_dml }}
-        enable_win_arm64: ${{ parameters.enable_win_arm64 }}
-        enable_macos_cpu: ${{ parameters.enable_macos_cpu }}
-        enable_android: ${{ parameters.enable_android }}
-        enable_apple_framework: ${{ parameters.enable_apple_framework }}
-        ort_version: ${{ parameters.ort_version }}
-        ort_cuda_version: ${{ parameters.ort_cuda_version }}
-        ort_dml_version: ${{ parameters.ort_dml_version }}
-        build_config: ${{ parameters.build_config }}
-        enable_win_qnn: ${{ parameters.enable_win_qnn }}
-        nuget_version_type: ${{ parameters.nuget_version_type }}
-
-    - ${{ if eq(parameters.enable_post_packaging_validation, true) }}:
-      - template: stages/nuget-validation-stage.yml
+      - template: stages/capi-packaging-stage.yml
         parameters:
           enable_win_cpu: ${{ parameters.enable_win_cpu }}
           enable_win_cuda: ${{ parameters.enable_win_cuda }}
@@ -170,14 +143,59 @@ extends:
           enable_win_dml: ${{ parameters.enable_win_dml }}
           enable_win_arm64: ${{ parameters.enable_win_arm64 }}
           enable_macos_cpu: ${{ parameters.enable_macos_cpu }}
+          enable_android: ${{ parameters.enable_android }}
+          enable_apple_framework: ${{ parameters.enable_apple_framework }}
           ort_version: ${{ parameters.ort_version }}
           ort_cuda_version: ${{ parameters.ort_cuda_version }}
           ort_dml_version: ${{ parameters.ort_dml_version }}
           cuda_version: ${{ parameters.cuda_version }}
-          SpecificArtifact: false
+          build_config: ${{ parameters.build_config }}
+          enable_winml: ${{ parameters.enable_winml }}
+          ort_winml_version: ${{ parameters.ort_winml_version }}
 
-    - ${{ if and(and(and(ne(parameters.custom_ort_package_versioned_name, 'None'), eq(parameters.enable_win_arm64, true)), eq(parameters.enable_win_cuda, true)), eq(parameters.enable_macos_cpu, true)) }}:
-      - template: stages/custom-nuget-packaging-stage.yml
-        parameters:
-          custom_ort_package_versioned_name: ${{ parameters.custom_ort_package_versioned_name }}
-          nuget_version_type: ${{ parameters.nuget_version_type }}
+      - ${{ if parameters.enable_winml }}:
+          - template: stages/winml-nuget-packaging-stage.yml
+            parameters:
+              build_config: ${{ parameters.build_config }}
+              nuget_version_type: ${{ parameters.nuget_version_type }}
+              ort_winml_version: ${{ parameters.ort_winml_version }}
+      - ${{ else }}:
+          - template: stages/nuget-packaging-stage.yml
+            parameters:
+              enable_win_cpu: ${{ parameters.enable_win_cpu }}
+              enable_win_cuda: ${{ parameters.enable_win_cuda }}
+              enable_linux_cpu: ${{ parameters.enable_linux_cpu }}
+              enable_linux_cuda: ${{ parameters.enable_linux_cuda }}
+              enable_win_dml: ${{ parameters.enable_win_dml }}
+              enable_win_arm64: ${{ parameters.enable_win_arm64 }}
+              enable_macos_cpu: ${{ parameters.enable_macos_cpu }}
+              enable_android: ${{ parameters.enable_android }}
+              enable_apple_framework: ${{ parameters.enable_apple_framework }}
+              ort_version: ${{ parameters.ort_version }}
+              ort_cuda_version: ${{ parameters.ort_cuda_version }}
+              ort_dml_version: ${{ parameters.ort_dml_version }}
+              build_config: ${{ parameters.build_config }}
+              enable_win_qnn: ${{ parameters.enable_win_qnn }}
+              nuget_version_type: ${{ parameters.nuget_version_type }}
+
+      - ${{ if eq(parameters.enable_post_packaging_validation, true) }}:
+          - template: stages/nuget-validation-stage.yml
+            parameters:
+              enable_win_cpu: ${{ parameters.enable_win_cpu }}
+              enable_win_cuda: ${{ parameters.enable_win_cuda }}
+              enable_linux_cpu: ${{ parameters.enable_linux_cpu }}
+              enable_linux_cuda: ${{ parameters.enable_linux_cuda }}
+              enable_win_dml: ${{ parameters.enable_win_dml }}
+              enable_win_arm64: ${{ parameters.enable_win_arm64 }}
+              enable_macos_cpu: ${{ parameters.enable_macos_cpu }}
+              ort_version: ${{ parameters.ort_version }}
+              ort_cuda_version: ${{ parameters.ort_cuda_version }}
+              ort_dml_version: ${{ parameters.ort_dml_version }}
+              cuda_version: ${{ parameters.cuda_version }}
+              SpecificArtifact: false
+
+      - ${{ if and(and(and(ne(parameters.custom_ort_package_versioned_name, 'None'), eq(parameters.enable_win_arm64, true)), eq(parameters.enable_win_cuda, true)), eq(parameters.enable_macos_cpu, true)) }}:
+          - template: stages/custom-nuget-packaging-stage.yml
+            parameters:
+              custom_ort_package_versioned_name: ${{ parameters.custom_ort_package_versioned_name }}
+              nuget_version_type: ${{ parameters.nuget_version_type }}

commit 8c363d5edc07d70ed4f8e9ccf25a9942515ce651

Fix TRT-RTX EP regression (#1754)
Fix regression caused by PR
https://github.com/microsoft/onnxruntime-genai/pull/1711

TRT-RTX EP was broken due to this regression

* Use right EP name for NvTensorRtRtx
* Make sure user_compute_stream option is set in WinML path

src/config.cpp
@@ -27,7 +27,7 @@ std::string_view NormalizeProviderName(std::string_view name) {
   } else if (lower_name == "vitisai") {
     return "VitisAI";
   } else if (lower_name == "nvtensorrtrtx") {
-    return "NvTensorRTRTX";
+    return "NvTensorRtRtx";
   }
   return name;  // Return name unchanged
 }

src/models/model.cpp
@@ -591,6 +591,14 @@ DeviceInterface* SetProviderSessionOptions(OrtSessionOptions& session_options,
           session_options.AddConfigEntry("ep.nvtensorrtrtxexecutionprovider.enable_cuda_graph", "1");
         }
         p_device = GetDeviceInterface(DeviceType::NvTensorRtRtx);
+
+        if (is_primary_session_options && p_device) {
+          void* stream_ptr = p_device->GetCudaStream();
+          std::stringstream stream_value;
+          stream_value << reinterpret_cast<uintptr_t>(stream_ptr);
+          std::string stream_value_str = stream_value.str();
+          session_options.AddConfigEntry("ep.nvtensorrtrtxexecutionprovider.user_compute_stream", stream_value_str.c_str());
+        }
       }
 
 #if USE_WINML
@@ -716,16 +724,6 @@ DeviceInterface* SetProviderSessionOptions(OrtSessionOptions& session_options,
       }
 #else
       std::vector<const char*> keys, values;
-      std::string stream_value_str;
-      if (provider_options.name == "NvTensorRtRtx" && is_primary_session_options && p_device) {
-        void* stream_ptr = p_device->GetCudaStream();
-        std::stringstream stream_value;
-        stream_value << reinterpret_cast<uintptr_t>(stream_ptr);
-        stream_value_str = stream_value.str();
-
-        keys.emplace_back("user_compute_stream");
-        values.emplace_back(stream_value_str.c_str());
-      }
 
       for (auto& option : provider_options.options) {
         keys.emplace_back(option.first.c_str());