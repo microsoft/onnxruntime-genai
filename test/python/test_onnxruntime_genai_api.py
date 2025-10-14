821fd6cc6f857bb14d4409c621eeae07df7b9864
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2025-10-14 08:25:27 +0000

    Add Python unit tests

--- test/python/test_onnxruntime_genai_api.py
+++ test/python/test_onnxruntime_genai_api.py
@@ -415,6 +415,16 @@ def test_e2e(device, phi2_for):
     model = og.Model(phi2_for(device))
     tokenizer = og.Tokenizer(model)
 
+    assert tokenizer.get_bos_token_id() == 50256
+    assert tokenizer.get_pad_token_id() == 50256
+    assert len(tokenizer.get_eos_token_id()) == 1
+    assert tokenizer.get_eos_token_id()[0] == 50256
+
+    assert tokenizer.get_bos_token() == "<|endoftext|>"
+    assert tokenizer.get_pad_token() == "<|endoftext|>"
+    assert len(tokenizer.get_eos_tokens()) == 1
+    assert tokenizer.get_eos_tokens()[0] == "<|endoftext|>"
+
     prompts = [
         "This is a test.",
     ]
