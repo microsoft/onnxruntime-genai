c0b9e99deebc803091fd5ba0c391f5bd9bdbe15c
Jiajia Qin <jiajiaqin@microsoft.com>
2026-03-11 14:04:04 +0800

    Revert "Use one generator for model_benchmark"
    This reverts commit 0fd50e839992b10ffe5ebcd3cc3864f41bdaca98.

--- benchmark/c/options.h
+++ benchmark/c/options.h
@@ -21,7 +21,6 @@ struct Options {
   size_t num_warmup_iterations{1};
   int64_t max_length{0};
   bool verbose{};
-  bool reuse_generator{};
 };
 
 Options ParseOptionsFromCommandLine(int argc, const char* const* argv);
