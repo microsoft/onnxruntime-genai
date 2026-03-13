59107138b5a8e24dccb3c7e262390562a5fa20f6
kinfey <kinfeylo@microsoft.com>
2026-03-13 21:34:06 +0800

    Update

--- src/python/py/models/builders/__init__.py
+++ src/python/py/models/builders/__init__.py
@@ -26,7 +26,7 @@ from .phi import (
     Phi4MMModel,
     PhiModel,
 )
-from .qwen import Qwen35Model, Qwen3Model, Qwen25VLTextModel, QwenModel
+from .qwen import Qwen35Model, Qwen3Model, Qwen3VLTextModel,Qwen25VLTextModel, QwenModel
 from .smollm import SmolLM3Model
 
 __all__ = [
