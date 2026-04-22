44c6fcc5a19a73a57dac72cdad0dd7873c771d0f
Jain, Vishal <vishal.jain@amd.com>
2026-04-22 18:56:12 +0530

    Update Copyright

--- src/python/py/models/builders/__init__.py
+++ src/python/py/models/builders/__init__.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License.  See License.txt in the project root for
 # license information.
 # -------------------------------------------------------------------------
-# Copyright (C) [2026] Advanced Micro Devices, Inc. All rights reserved.
+# Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
 # Portions of this file consist of AI generated content.
 # -------------------------------------------------------------------------
 from .base import Model
