23a5462cc252654b5f4e2a2c06130c79406f5fac
Jain, Vishal <vishal.jain@amd.com>
2026-04-22 18:55:40 +0530

    Update Copyright notice for modified file

--- src/python/py/models/builders/qwen.py
+++ src/python/py/models/builders/qwen.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # Licensed under the MIT License.  See License.txt in the project root for
 # license information.
-# --------------------------------------------------------------------------
+# ------------------------------------------------------
 # Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
 # Portions of this file consist of AI generated content.
 
