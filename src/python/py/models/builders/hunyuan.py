f46ca8a109c5e384b029aff284f69ac71c6febf8
Jain, Vishal <vishal.jain@amd.com>
2026-04-23 15:55:27 +0530

    Add spdx notice

--- src/python/py/models/builders/hunyuan.py
+++ src/python/py/models/builders/hunyuan.py
@@ -1,6 +1,9 @@
 # -------------------------------------------------------------------------
 # Copyright (C) [2026] Advanced Micro Devices, Inc. All rights reserved.
 # Portions of this file consist of AI generated content.
+#
+# SPDX-License-Identifier: MIT
+#
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
