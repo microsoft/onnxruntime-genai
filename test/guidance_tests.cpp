0889c740563c226a7e2cdc4763cee5cdbbe3db2a
Jiajia Qin <jiajiaqin@microsoft.com>
2026-04-27 09:02:01 +0800

    Fix clang-format whitespace in guidance_tests.cpp

--- test/guidance_tests.cpp
+++ test/guidance_tests.cpp
@@ -69,7 +69,7 @@ TEST_F(GuidanceTests, UseRegex) {
   EXPECT_TRUE(std::regex_match(output, std::regex("answer: .*")));
 }
 
-#if 0  // Temporarily disable JSON schema and LARK grammar tests
+#if 0   // Temporarily disable JSON schema and LARK grammar tests
 TEST_F(GuidanceTests, UseLarkGrammarSingleTurn) {
   auto input_string = get_qwen_2_5_prompt("What is the weather in Seattle?");
   auto input_sequences = OgaSequences::Create();
@@ -169,7 +169,7 @@ TEST_F(GuidanceTests, UseJsonSchemaMultiTurn) {
     EXPECT_EQ(output, expected_output);
   }
 }
-#endif        // #if 0
+#endif  // #if 0
 
 #endif  // TEST_QWEN_2_5
 
