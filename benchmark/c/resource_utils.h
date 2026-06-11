e4aaf0015d28a18b7beaf47f2df2f986fe232d3c
Sushanth Rajasankar <sushraja@microsoft.com>
2026-06-11 11:35:35 -0700

    Model benchmark improvements.

--- benchmark/c/resource_utils.h
+++ benchmark/c/resource_utils.h
@@ -9,4 +9,12 @@ namespace benchmark::utils {
 
 size_t GetPeakWorkingSetSizeInBytes();
 
+struct GpuMemoryInfo {
+  size_t dedicated;  // VRAM (DXGI_MEMORY_SEGMENT_GROUP_LOCAL)
+  size_t shared;     // System RAM used by GPU (DXGI_MEMORY_SEGMENT_GROUP_NON_LOCAL)
+  size_t Total() const { return dedicated + shared; }
+};
+
+GpuMemoryInfo GetGpuMemoryUsage();
+
 }  // namespace benchmark::utils
