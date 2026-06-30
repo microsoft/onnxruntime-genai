a3e157f657179a15319460c536379fc5525e2c00
Samyak Shrimali <t-sshrimali@microsoft.com>
2026-06-30 12:51:10 -0700

    Validate TransducerState downcast before binding reference

--- src/transducer_decoding_strategy.cpp
+++ src/transducer_decoding_strategy.cpp
@@ -7,8 +7,17 @@
 
 namespace Generators {
 
+// Validate the downcast before binding the non-null reference member
+static TransducerState& RequireTransducerState(Generator& g) {
+  auto* transducer_state = dynamic_cast<TransducerState*>(g.state_.get());
+  if (!transducer_state) {
+    throw std::runtime_error("TransducerDecodingStrategy requires TransducerState");
+  }
+  return *transducer_state;
+}
+
 TransducerDecodingStrategy::TransducerDecodingStrategy(Generator& g)
-    : transducer_state_{*dynamic_cast<TransducerState*>(g.state_.get())} {}
+    : transducer_state_{RequireTransducerState(g)} {}
 
 void TransducerDecodingStrategy::Step(Generator& g) {
   g.state_->SetExtraInputs(g.extra_inputs_);
