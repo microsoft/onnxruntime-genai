2c1948819d764c07274939af4e71d8095f198941
Daniel Song <danielsong@microsoft.com>
2026-08-12 13:38:01 -0700

    Validate static batching configuration

---
+++ test/engine/config_tests.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <gtest/gtest.h>
+
+#include "config.h"
+
+namespace Generators::test {
+
+TEST(ConfigTest, ParsesStaticBatching) {
+  Config config;
+
+  OverlayConfig(config, R"({"engine":{"static_batching":{"max_batch_size":8}}})");
+
+  ASSERT_TRUE(config.engine.static_batching.has_value());
+  EXPECT_EQ(config.engine.static_batching->max_batch_size, 8u);
+}
+
+TEST(ConfigTest, RejectsNonPositiveStaticBatchSize) {
+  Config config;
+
+  try {
+    OverlayConfig(config, R"({"engine":{"static_batching":{"max_batch_size":0}}})");
+    FAIL() << "Expected invalid max_batch_size to throw";
+  } catch (const std::runtime_error& error) {
+    const std::string message = error.what();
+    EXPECT_NE(message.find("engine:static_batching:max_batch_size:"), std::string::npos) << message;
+  }
+}
+
+}  // namespace Generators::test
