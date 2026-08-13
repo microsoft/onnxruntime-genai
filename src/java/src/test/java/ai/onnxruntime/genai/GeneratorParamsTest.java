d9de80ac24e9dff80591bdf036a3641db06495af
Samyak Shrimali <t-sshrimali@microsoft.com>
2026-08-13 15:52:58 -0700

    Fix for java test issues

--- src/java/src/test/java/ai/onnxruntime/genai/GeneratorParamsTest.java
+++ src/java/src/test/java/ai/onnxruntime/genai/GeneratorParamsTest.java
@@ -6,7 +6,6 @@ package ai.onnxruntime.genai;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
