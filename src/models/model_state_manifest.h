152d0623ed137118c7ef2c76208eeed36cfcae86
Baiju Meswani <bmeswani@microsoft.com>
2026-08-19 16:35:27 +0000

    Add decoder state manifest contract for hybrid model support
    (cherry picked from commit 0061d8ee724bb3b826d11cee35c1c3956776cc85)

---
+++ src/models/model_state_manifest.h
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "../config.h"
+
+#include "onnxruntime_c_api.h"
+
+namespace Generators {
+
+struct ModelStateMetadata {
+  virtual ~ModelStateMetadata() = default;
+
+  virtual bool HasInput(const std::string& name) const = 0;
+  virtual bool HasOutput(const std::string& name) const = 0;
+  virtual ONNXTensorElementDataType GetInputDataType(const std::string& name) const = 0;
+  virtual ONNXTensorElementDataType GetOutputDataType(const std::string& name) const = 0;
+  virtual std::vector<int64_t> GetInputShape(const std::string& name) const = 0;
+  virtual std::vector<int64_t> GetOutputShape(const std::string& name) const = 0;
+};
+
+class ModelStateManifest {
+ public:
+  explicit ModelStateManifest(const Config::Model::Decoder& decoder);
+
+  static void ValidateConfig(const Config::Model::Decoder& decoder);
+  static void ValidateDynamicEngineCompatibility(const Config::Model::Decoder& decoder);
+  void ValidateSession(const ModelStateMetadata& metadata) const;
+
+ private:
+  std::vector<Config::Model::Decoder::StateGroup> state_groups_;
+};
+
+}  // namespace Generators
