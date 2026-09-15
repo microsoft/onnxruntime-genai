106704f228b4376f93e1bd1ae9e53dae10fe8752
Anirudh Swaminathan <anirudh.swaminathan@intel.com>
2026-09-15 11:26:49 -0700

    Remove phi3v from RewindToLength exclusion

--- src/generator/generators.cpp
+++ src/generator/generators.cpp
@@ -962,7 +962,7 @@ SpeculativeStats Generator::GetSpeculativeStats() const {
 }
 
 void Generator::RewindToLength(size_t new_length) {
-  if (model_->config_->model.type == "whisper" || model_->config_->model.type == "phi3v" || model_->config_->model.type == "decoder-pipeline" || model_->config_->model.type == "lfm2")
+  if (model_->config_->model.type == "whisper" || model_->config_->model.type == "decoder-pipeline" || model_->config_->model.type == "lfm2")
     throw std::runtime_error("RewindTo is currently not supported for " + model_->config_->model.type + ".");
   const size_t current_length = search_->GetSequenceLength();
   if (new_length > current_length)
